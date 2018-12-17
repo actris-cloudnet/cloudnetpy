droplet derivative fix

cloudnetpy/classify.py
@@ -3,8 +3,8 @@
 # import sys
 import numpy as np
 import numpy.ma as ma
-# import matplotlib as mpl
-# import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from scipy import stats
 from cloudnetpy import droplet

cloudnetpy/droplet.py
@@ -1,6 +1,7 @@
 """ This module has functions for liquid layer detection.
 """
 
+import sys
 import numpy as np
 import numpy.ma as ma
 import scipy.signal
@@ -73,7 +74,7 @@ def get_top_ind(dprof, p, nprof, dist, lim):
 
 
 def get_liquid_layers(beta, height, peak_amp=2e-5, max_width=300,
-                      min_points=3, min_top_der=4e-7):
+                      min_points=3, min_top_der=1e6):
     """ Estimate liquid layers from SNR-screened attenuated backscattering.
 
     Args:
@@ -82,7 +83,9 @@ def get_liquid_layers(beta, height, peak_amp=2e-5, max_width=300,
         peak_amp (float, optional): Minimum value for peak. Default is 2e-5.
         max_width (float, optional): Maximum width of peak. Default is 300 (m).
         min_points (int, optional): Minimum number of valid points in peak. Default is 3.
-        min_top_der (float, optional): Minimum derivative above peak. Default is 4e-7.
+        min_top_der (float, optional): Minimum derivative above peak 
+            defined as (alt_top-alt_peak)/(beta_peak-beta_top) which 
+            is always positive. Default is 1e6.
 
     Returns:
         (array_like): Classification of liquid at each point: 1 = Yes,  0 = No
@@ -92,17 +95,12 @@ def get_liquid_layers(beta, height, peak_amp=2e-5, max_width=300,
     dheight = utils.med_diff(height)
     base_below_peak = int(np.ceil((200/dheight)))
     top_above_peak = int(np.ceil((150/dheight)))
-    # init result matrices
     cloud_bit = ma.masked_all(beta.shape, dtype=int)
-    # set missing values to 0
-    beta_diff = np.diff(beta, axis=1).filled(fill_value=0)  # difference matrix
-    beta = beta.filled(fill_value=0)
-    # all peaks
+    beta_diff = np.diff(beta, axis=1).filled(0)  # difference matrix
+    beta = beta.filled(0)
     pind = scipy.signal.argrelextrema(beta, np.greater, order=4, axis=1)
-    # strong peaks
     strong_peaks = np.where(beta[pind] > peak_amp)
     pind = (pind[0][strong_peaks], pind[1][strong_peaks])
-    # loop over strong peaks
     for n, p in zip(*pind):
         lprof = beta[n, :]
         dprof = beta_diff[n, :]
@@ -113,12 +111,13 @@ def get_liquid_layers(beta, height, peak_amp=2e-5, max_width=300,
         try:
             top = get_top_ind(dprof, p, height.shape[0], top_above_peak, 4)
         except:
-            continue
-        tval, pval = lprof[top], lprof[p]
-        # calculate peak properties
+            continue      
         npoints = np.count_nonzero(lprof[base:top+1])
         peak_width = height[top] - height[base]
-        topder = (pval - tval) / peak_width
-        if (npoints > min_points and peak_width < max_width and topder > min_top_der):
+        top_der = (height[top] - height[p]) / (lprof[p] - lprof[top])
+        conds = (npoints > min_points,
+                 peak_width < max_width,
+                 top_der > min_top_der)
+        if all(conds):
             cloud_bit[n, base:top+1] = 1
     return cloud_bit