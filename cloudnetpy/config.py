bc034f53cf9b3bb5b2f03dddceb7b97446be74a5
Simo Tukiainen <simo.tukiainen@fmi.fi>
2018-12-23 13:30:02 +0200

    congig change

--- cloudnetpy/config.py
+++ cloudnetpy/config.py
@@ -15,7 +15,8 @@ LWP_ERROR = (0.25, 20)
 # Estimated precision of gas attenuation
 GAS_ATTEN_PREC = 0.1
 
-BETA_BIAS = 3.0
-BETA_ERROR = 0.5
+# Random error and bias in lidar backscattering
+BETA_ERROR = (0.5, 3.0)
 
+# Estimate of the bias in cloud radar echo
 Z_BIAS = 1
