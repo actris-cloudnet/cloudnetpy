63e056461aa4ab7a3feb48be0ae2515907aca989
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-01-31 13:15:26 +0200

    rpg reader works for l1 data

--- cloudnetpy/rpg.py
+++ cloudnetpy/rpg.py
@@ -1,6 +1,6 @@
+from collections import namedtuple
 import numpy as np
 import numpy.ma as ma
-from collections import namedtuple
 
 
 class Rpg:
@@ -37,7 +37,7 @@ class Rpg:
         return False
 
     def read_rpg_header(self):
-
+        """Reads the header or rpg binary file."""
         def insert(names, dtype=np.int32, n_values=1):
             for name in names:
                 header[name] = np.fromfile(f, dtype, int(n_values))
@@ -99,14 +99,14 @@ class Rpg:
         return header
 
     def read_rpg_data(self):
-        """Read the binary data."""
-
+        """Read the actual data from rpg binary file."""
         Dimensions = namedtuple('Dimensions', ['n_samples',
                                                'n_gates',
                                                'n_layers_t',
                                                'n_layers_h'])
 
         def create_dimensions():
+            """Loop lengths in the data read."""
             n_samples = np.fromfile(f, np.int32, 1)
             return Dimensions(int(n_samples),
                               int(self.header['NumbGates']),
@@ -114,94 +114,110 @@ class Rpg:
                               int(self.header['NumbLayersH']))
 
         def create_shapes():
+            """Possible shapes of the data arrays."""
             return((dims.n_samples,),
                    (dims.n_samples, dims.n_layers_t),
                    (dims.n_samples, dims.n_layers_h),
                    (dims.n_samples, dims.n_gates))
 
-        def create_meta():
-            """Variable names, dimensions and input/output data types.
+        def create_variables():
+            """Variable names, data arrays and input data types.
 
             These need to be defined in the same order as they appear in
             the file.
 
             """
             shapes = create_shapes()
-            vars = {}
-            vars['SampBytes'] = (shapes[0], np.int32, np.int)
-            vars['Time'] = (shapes[0], np.uint32, np.int)
-            vars['Time_usec'] = (shapes[0], np.int32, np.int)
-            vars['QF'] = (shapes[0], np.int8, np.int)
-
+            fun = np.zeros
+            vrs = {}
+            vrs['SampBytes'] = (fun(shapes[0], np.int), np.int32)
+            vrs['Time'] = (fun(shapes[0], np.int), np.uint32)
+            vrs['Time_usec'] = (fun(shapes[0], np.int), np.int32)
+            vrs['QF'] = (fun(shapes[0], np.int), np.int8)
             for var_name in ('RR', 'RH', 'T', 'P', 'WS', 'WD', 'DD_V', 'Tb',
                              'LWP', 'PowIF', 'El', 'Az', 'BlwStatus',
                              'TransPow', 'TransT', 'RecT', 'PCT'):
-                vars[var_name] = (shapes[0], np.float32, np.float)
-
-            vars['T_Prof'] = (shapes[1], np.float32, np.float)
-
+                vrs[var_name] = (fun(shapes[0]), np.float32)
+            vrs['T_Prof'] = (fun(shapes[1]), np.float32)
             for var_name in ('AbsHumid_Prof', 'RH_Prof'):
-                vars[var_name] = (shapes[2], np.float32, np.float)
-
+                vrs[var_name] = (fun(shapes[2]), np.float32)
             for var_name in ('Sensit_v', 'Sensit_h'):
-                vars[var_name] = (shapes[3], np.float32, np.float)
-
-            vars['PrMsk'] = (shapes[3], np.int8, np.int)
-
+                vrs[var_name] = (fun(shapes[3]), np.float32)
+            vrs['PrMsk'] = (fun(shapes[3], np.int), np.int8)
             for var_name in ('Zv', 'Vel', 'SW', 'Skew', 'Kurt', 'LDR', 'CorrC',
                              'PhiX'):
-                vars[var_name] = (shapes[3], np.float32, np.float)
-
-            return vars
-
-        def create_data():
-            """Data dictionary."""
-            data_out = {}
-            for name in meta:
-                shape, _, dtype = meta[name][:]
-                data_out[name] = ma.masked_all(shape, dtype=dtype)
-            return data_out
-
-        def keyrange(key1, key2):
-            """Indices of dict fields from one key to another."""
-            all_keys = list(data.keys())
-            first_index = all_keys.index(key1)
-            second_index = all_keys.index(key2)
-            return all_keys[first_index:second_index+1]
+                vrs[var_name] = (fun(shapes[3]), np.float32)
+            return vrs
 
         def append(name, n_elements):
-            x = np.fromfile(f, meta[name][1], n_elements)
-            if n_elements == 1:
-                data[name][sample] = x
+            """Append data into already allocated arrays."""
+            array, dtype = data[name]
+            values = np.fromfile(f, dtype, n_elements)
+            if n_elements == 1 and array.ndim == 1:
+                array[sample] = values
+            elif n_elements == 1 and array.ndim == 2:
+                array[sample][gate] = values
             else:
-                data[name][sample][:] = x
+                array[sample][:] = values
+
+        def get_keyranges():
+            """Returns dict-names for the different 'groups' of variables.
+
+            The variables are grouped in the binary file into 5 groups.
+            The keyranges make it easy to separate these groups once
+            you know the first and last variable name in each group.
+
+            """
+            def _keyrange(key1, key2):
+                """List of keys from one key to another."""
+                ind1 = keys.index(key1)
+                ind2 = keys.index(key2)
+                return keys[ind1:ind2 + 1]
+
+            keys = list(data.keys())
+            return (_keyrange('SampBytes', 'PCT'),
+                    _keyrange('AbsHumid_Prof', 'RH_Prof'),
+                    _keyrange('Sensit_v', 'PrMsk'),
+                    _keyrange('Zv', 'Kurt'),
+                    _keyrange('LDR', 'PhiX'))
+
+        def _fix_output():
+            """Returns just the data arrays as MaskedArrays."""
+            out = {}
+            for name in data:
+                out[name] = ma.masked_equal(data[name][0], 0)
+            return out
 
         f = open(self.filename, 'rb')
         f.seek(self.file_position)
         dims = create_dimensions()
-        meta = create_meta()
-        data = create_data()
+        data = create_variables()
+        keyranges = get_keyranges()
 
         for sample in range(dims.n_samples):
-            for key in keyrange('SampBytes', 'PCT'):
+
+            for key in keyranges[0]:
                 append(key, 1)
+
             _ = np.fromfile(f, np.int32, 3)
+
             append('T_Prof', dims.n_layers_t)
-            for key in keyrange('AbsHumid_Prof', 'RH_Prof'):
+
+            for key in keyranges[1]:
                 append(key, dims.n_layers_h)
-            for key in keyrange('Sensit_v', 'PrMsk'):
+
+            for key in keyranges[2]:
                 append(key, dims.n_gates)
 
-            for key, value in data.items():
-                print(key,value)
-
             for gate in range(dims.n_gates):
-                if data['PrMsk'][sample][gate] == 1:
-                    for key in keyrange('Zv', 'Kurt'):
+
+                if data['PrMsk'][0][sample][gate] == 1:
+                    for key in keyranges[3]:
                         append(key, 1)
+
                     if self.dual_pol:
-                        append('LDR', 1)
-                        append('PhiX', 1)
+                        for key in keyranges[4]:
+                            append(key, 1)
         f.close()
-        return data
+        return _fix_output()
 
