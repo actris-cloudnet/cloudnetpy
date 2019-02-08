b3a09e9169272829064e1a4dc16ad4b904f36dd0
Anniina Korpinen <anniina.korpinen@fmi.fi>
2019-02-08 15:05:59 +0200

    trash clean up 2

--- cloudnetpy/products/bits.py
+++ cloudnetpy/products/bits.py
@@ -4,7 +4,8 @@ def is_bit(n, k):
     """ element wise test of bit k (1,2,3...) on integer n """
     mask = 1 << k-1
     return (n & mask > 0)
-                                                                                                                                             
+
+                                                                                                                                           
 def set_bit(n, k):
     """ set bit k (1,2,3..) on integer n """
     mask = 1 << k-1
