338a4fb0d9162e55dffc56b46331023f701ee34e
Anniina Korpinen <anniina.korpinen@fmi.fi>
2019-02-14 11:51:51 +0200

    more fixing bugs

--- cloudnetpy/products/test_classification.py
+++ cloudnetpy/products/test_classification.py
@@ -7,7 +7,8 @@ from cloudnetpy.products.ncf import save_Cnet
 #fname = '20170608_lindenberg_categorize.nc'
 #fname = '20170927_ny-alesund_categorize.nc'
 fname = '/home/korpinen/Documents/ACTRIS/cloudnet_data/20190113_juelich_categorize.nc'
+outname = '/home/korpinen/Documents/ACTRIS/cloudnetpy/test_data.nc'
 #fname = 'data/20180110_mace-head_categorize.nc'
 
 # generate classification
-generate_class(fname)
+generate_class(fname,outname)
