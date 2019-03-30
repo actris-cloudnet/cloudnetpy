82cbaa011012c864c2c2f9539909413f56f9ff87
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-03-30 20:46:35 +0200

    small refactoring

--- cloudnetpy/ceilo.py
+++ cloudnetpy/ceilo.py
@@ -1,3 +1,4 @@
+"""Module for reading and processing Vaisala ceilometers."""
 import linecache
 import numpy as np
 
@@ -6,14 +7,20 @@ class VaisalaCeilo:
     """Base class for Vaisala ceilometers."""
     def __init__(self, file_name):
         self.file_name = file_name
-        self.params = None
+        self._params = None
         self.backscatter = None
         self.metadata = None
+        self.range = None
+
+    def _calc_range(self):
+        n_gates = int(self.metadata['number_of_gates'][0])
+        range_resolution = int(self.metadata['range_resolution'][0])
+        return np.arange(1, n_gates + 1) * range_resolution
 
     def _read_lines_in_file(self):
         """Returns file contents as list, one line per cell."""
-        with open(self.file_name) as f:
-            return f.readlines()
+        with open(self.file_name) as file:
+            return file.readlines()
 
     @staticmethod
     def _get_empty_lines(data):
@@ -21,7 +28,7 @@ class VaisalaCeilo:
         return [n for n, _ in enumerate(data) if isempty(data[n])]
 
     def _convert_data(self, lines):
-        n_chars = self.params[0]
+        n_chars = self._params[0]
         n_gates = int((len(lines[0])-1)/n_chars)
         profiles = np.zeros((len(lines), n_gates), dtype=int)
         ran = range(0, n_gates*n_chars, n_chars)
@@ -31,35 +38,40 @@ class VaisalaCeilo:
             except ValueError as error:
                 print(error)
                 profiles[ind, :] = np.zeros(n_gates, dtype=int)
-        ind = np.where(profiles & self.params[1] != 0)
-        profiles[ind] -= self.params[2]
+        ind = np.where(profiles & self._params[1] != 0)
+        profiles[ind] -= self._params[2]
         return profiles
 
 
 class ClCeilo(VaisalaCeilo):
-    """Base class for more modern Vaisala CL31/CL51 ceilometers."""
+    """Base class for Vaisala CL31/CL51 ceilometers."""
     def __init__(self, file_name):
         super().__init__(file_name)
-        self.params = (5, 524288, 1048576)
+        self._params = (5, 524288, 1048576)
 
     def read_data(self):
         """Read all lines of data from the file."""
         all_lines = self._read_lines_in_file()
         empty_lines = self._get_empty_lines(all_lines)
         data_lines = self._parse_lines(all_lines, empty_lines)
-        header_line1 = self._read_header_line_1(data_lines[1])
-        header_line2 = self._read_header_line_2(data_lines[2])
-        header_line4 = self._read_header_line_4(data_lines[-2])
-        self.metadata = {**header_line1, **header_line2, **header_line4}
+        self.metadata = self._read_metadata(data_lines)
         self.backscatter = self._convert_data(data_lines[-1])
+        self.range = self._calc_range()
+
+    @classmethod
+    def _read_metadata(cls, data_lines):
+        header_line1 = cls._read_header_line_1(data_lines[1])
+        header_line2 = cls._read_header_line_2(data_lines[2])
+        header_line4 = cls._read_header_line_4(data_lines[-2])
+        return {**header_line1, **header_line2, **header_line4}
 
     @staticmethod
     def _parse_lines(data, empty_lines):
         number_of_data_lines = empty_lines[1] - empty_lines[0] - 1
         lines = [[data[n+1][1:-1] for n in empty_lines],  # time stamp
                  [data[n+2][1:-2] for n in empty_lines]]
-        for m in range(3, number_of_data_lines):
-            lines.append([data[n+m] for n in empty_lines])
+        for line_number in range(3, number_of_data_lines):
+            lines.append([data[n+line_number] for n in empty_lines])
         return lines
 
     @staticmethod
@@ -99,18 +111,21 @@ def _values_to_dict(keys, values):
 
 
 class Cl51(ClCeilo):
+    """Class for Vaisala CL51 ceilometer."""
     def __init__(self, input_file):
         super().__init__(input_file)
         self.model = 'cl51'
 
 
 class Cl31(ClCeilo):
+    """Class for Vaisala CL31 ceilometer."""
     def __init__(self, input_file):
         super().__init__(input_file)
         self.model = 'cl31'
 
 
 class Ct25k(VaisalaCeilo):
+    """Class for Vaisala CT25k ceilometer."""
     def __init__(self, input_file):
         super().__init__(input_file)
         self.model = 'ct25k'
@@ -121,8 +136,9 @@ class Ct25k(VaisalaCeilo):
 
 
 def ceilo2nc(input_file, output_file):
-    cl = _initialize_ceilo(input_file)
-    cl.read_data()
+    """Converts Vaisala ceilometer txt-file to netCDF."""
+    ceilo = _initialize_ceilo(input_file)
+    ceilo.read_data()
 
 
 def _initialize_ceilo(file):
@@ -143,18 +159,21 @@ def _find_ceilo_model(file):
         return 'cl31'
     elif hint == 'CT02':
         return 'ct25k'
+    return None
 
 
-def _find_first_empty_line(file):
-    line_number = 0
-    while True:
-        line = linecache.getline(file, line_number)
-        if isempty(line):
-            return line_number
-        line_number += 1
+def _find_first_empty_line(file_name):
+    line_number = 1
+    with open(file_name) as file:
+        for line in file:
+            if isempty(line):
+                break
+            line_number += 1
+    return line_number
 
 
 def isempty(line):
+    """Tests if line in text file is empty."""
     if line in ('\n', '\r\n'):
         return True
     return False
