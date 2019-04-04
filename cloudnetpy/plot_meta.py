979b28295e7e433efe432398c8bf9307ba188f8f
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-04-04 10:43:21 +0300

    plotting meta clean up

--- cloudnetpy/plot_meta.py
+++ cloudnetpy/plot_meta.py
@@ -4,8 +4,6 @@ from collections import namedtuple
 FIELDS = ('name',
           'cbar',
           'clabel',
-          'xlabel',
-          'ylabel',
           'plot_range',
           'plot_scale',
           'plot_type')
@@ -14,6 +12,8 @@ PlotMeta = namedtuple('PlotMeta', FIELDS, defaults=(None,)*len(FIELDS))
 
 _LOG = 'logarithmic'
 _LIN = 'linear'
+_KGM2 = '$kg$'+' $m^{-2}$'
+_KGM3 = '$kg$'+' $m^{-3}$'
 
 _CLABEL = {
     'target_classification':
@@ -69,123 +69,96 @@ _CBAR = {
         ("#f8f8ff", "#ffff00", "#2f4f4f", "#3cb371", "#778899", "#87cefa",
          "#d3d3d3", "#0000ff", "#ff4500", "#ffa500"),
 
-    'iwc': 'jet',
-    'iwc_error': 'jet',
-
     'iwc_retrieval_status':
         ("#f8f8ff", "#00bfff", "#ff4500", "#0000ff", "#ffff00",
          "#2f4f4f", "#778899", "#d3d3d3"),
 
-    'iwc_sensitivity': 'jet',
-    'iwc_bias': 'jet',
-    'iwc_inc_rain': 'jet',
-
-    'lwc': 'jet',
-    'lwc_error': 'jet',
-    'lwc_th': 'jet',
-    
     'lwc_retrieval_status':
         ("#f8f8ff", "#00bfff", "#0000ff", "#2f4f4f", "#ffa500", 
          "#ff4500", "#2f4f4f"),
-    
-    'lwp': 'jet',
-    'lwp_error': 'jet'
 }
 
 ATTRIBUTES = {
     'target_classification': PlotMeta(
-        'Target classification',
+        name='Target classification',
         cbar=_CBAR['target_classification'],
         clabel=_CLABEL['target_classification'],
         plot_type='segment'
     ),
     'detection_status': PlotMeta(
-        'Radar and lidar detection status',
+        name='Radar and lidar detection status',
         cbar=_CBAR['detection_status'],
         clabel=_CLABEL['detection_status'],
         plot_type='segment'
     ),
     'iwc': PlotMeta(
-        'Ice water content',
-        cbar=_CBAR['iwc'],
-        clabel='$kg$'+' $m^{-3}$',
+        name='Ice water content',
+        cbar='jet',
+        clabel=_KGM3,
+        plot_range=(1e-7, 1e-3),
+        plot_scale=_LOG,
+        plot_type='mesh'
+    ),
+    'iwc_inc_rain': PlotMeta(
+        name='Ice water content including rain',
+        cbar='jet',
+        clabel=_KGM3,
         plot_range=(1e-7, 1e-3),
         plot_scale=_LOG,
         plot_type='mesh'
     ),
     'iwc_error': PlotMeta(
-        'Random error in ice water content, one standard deviation',
-        cbar=_CBAR['iwc_error'],
+        name='Random error in ice water content, one standard deviation',
+        cbar='jet',
         clabel='dB',
         plot_range=(0, 3),
         plot_scale=_LIN,
         plot_type='mesh'
     ),
-    'iwc_bias': PlotMeta(
-        'Possible bias in ice water content, one standard deviation',
-        clabel='dB',
-        plot_type='mesh'
-    ),
-    'iwc_sensitivity': PlotMeta(
-        'Minimum detectable ice water content',
-        clabel='$kg$'+' $m^{-3}$',
-        plot_type='mesh'
-    ),
     'iwc_retrieval_status': PlotMeta(
-        'Ice water content retrieval status',
+        name='Ice water content retrieval status',
         cbar=_CBAR['iwc_retrieval_status'],
         clabel=_CLABEL['iwc_retrieval_status'],
         plot_type='segment'
     ),
-    'iwc_inc_rain': PlotMeta(
-        'Ice water content including rain',
-        cbar=_CBAR['iwc_inc_rain'],
-        clabel='$kg$'+' $m^{-3}$',
-        plot_range=(1e-7, 1e-3),
-        plot_scale=_LOG,
-        plot_type='mesh'
+    'iwc_bias': PlotMeta(
+        name='Possible bias in ice water content, one standard deviation',
+        clabel='dB',
+    ),
+    'iwc_sensitivity': PlotMeta(
+        name='Minimum detectable ice water content',
+        clabel=_KGM3,
     ),
     'lwc': PlotMeta(
-        'Liquid water content',
-        cbar=_CBAR['lwc'],
-        clabel='$kg$'+' $m^{-3}$',
+        name='Liquid water content',
+        cbar='jet',
+        clabel=_KGM3,
         plot_range=(1e-5, 1e-2),
         plot_scale=_LOG,
         plot_type='mesh'
     ),
     'lwc_error': PlotMeta(
-        'Random error in liquid water content, one standard deviation',
+        name='Random error in liquid water content, one standard deviation',
+        cbar='jet',
         clabel='dB',
         plot_range=(0, 2),
         plot_scale=_LIN,
         plot_type='mesh'
     ),
     'lwc_retrieval_status': PlotMeta(
-        'Liquid water content retrieval status',
+        name='Liquid water content retrieval status',
         cbar=_CBAR['lwc_retrieval_status'],
         clabel=_CLABEL['lwc_retrieval_status'],
         plot_type='segment'
     ),
     'lwp': PlotMeta(
-        'Liquid water path',
-        cbar=_CBAR['lwp'],
-        clabel='$kg$'+' $m^{-2}$',
-        plot_range=(-100, 1000),
+        name='Liquid water path',
+        clabel=_KGM2,
+        plot_range=(0, 1),
         plot_scale=_LIN,
-        plot_type='mesh'
     ),
     'lwp_error': PlotMeta(
-        'Random error in liquid water path, one standard deviation',
-        cbar=_CBAR['lwp_error'],
-        clabel='$kg$'+' $m^{-2}$',
-        plot_type='mesh'
+        name='Random error in liquid water path, one standard deviation',
+        clabel=_KGM2,
     ),
-    'lwc_th': PlotMeta(
-        'Liquid water content (tophat distribution)',
-        cbar=_CBAR['lwc_th'],
-        clabel='$kg$'+' $m^{-3}$',
-        plot_range=(1e-5, 1e-2),
-        plot_scale=_LOG,
-        plot_type='mesh'
-    )
 }
