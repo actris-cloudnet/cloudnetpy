f7abb95e80c45a70dd4b60513ddbc2c5776ffa76
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-04-26 10:36:02 +0300

    beta calculation inside ceilometer class

--- cloudnetpy/ceilo.py
+++ cloudnetpy/ceilo.py
@@ -16,6 +16,8 @@ SECONDS_IN_HOUR = 3600
 
 
 class Ceilometer:
+    """Base class for all types of ceilometers."""
+
     def __init__(self, file_name):
         self.file_name = file_name
         self.model = None
@@ -26,6 +28,105 @@ class Ceilometer:
         self.date = None
         self.noise_params = None
         self.data = {}
+
+    def calc_beta(self):
+        """Converts range-corrected raw beta to noise-screened beta."""
+
+        def _screen_beta(beta_in, smooth):
+            beta_in = self._calc_range_uncorrected_beta(beta_in, range_squared)
+            beta_in = self._screen_by_snr(beta_in, is_saturation, smooth=smooth)
+            return self._calc_range_corrected_beta(beta_in, range_squared)
+
+        range_squared = self._get_range_squared()
+        is_saturation = self._find_saturated_profiles()
+        beta = _screen_beta(self.backscatter, False)
+        # smoothed version:
+        beta_smooth = ma.copy(self.backscatter)
+        cloud_ind, cloud_values, cloud_limit = self._estimate_clouds_from_beta(beta)
+        beta_smooth[cloud_ind] = cloud_limit
+        sigma = self._calc_sigma_units()
+        beta_smooth = scipy.ndimage.filters.gaussian_filter(beta_smooth, sigma)
+        beta_smooth[cloud_ind] = cloud_values
+        beta_smooth = _screen_beta(beta_smooth, True)
+        return self.backscatter, beta, beta_smooth
+
+    @staticmethod
+    def _estimate_clouds_from_beta(beta):
+        """Naively finds strong clouds from ceilometer backscatter."""
+        cloud_limit = 1e-6
+        cloud_ind = np.where(beta > cloud_limit)
+        return cloud_ind, beta[cloud_ind], cloud_limit
+
+    def _screen_by_snr(self, beta_uncorrected, is_saturation, smooth=False):
+        """Screens noise from ceilometer backscatter.
+
+        Args:
+            beta_uncorrected (ndarray): Range-uncorrected backscatter.
+            ceilo (obj): Ceilometer object.
+            is_saturation (ndarray): Boolean array denoting saturated profiles.
+            smooth (bool): Should be true if input beta is smoothed. Default is False.
+
+        """
+        beta = ma.copy(beta_uncorrected)
+        n_gates, _, saturation_noise, noise_min = self.noise_params
+        noise_min = noise_min[0] if smooth else noise_min[1]
+        noise = self._estimate_noise_from_top_gates(beta, n_gates, noise_min)
+        beta = self._reset_low_values_above_saturation(beta, is_saturation, saturation_noise)
+        beta = self._remove_noise(beta, noise)
+        return beta
+
+    @staticmethod
+    def _estimate_noise_from_top_gates(beta, n_gates, noise_min):
+        """Estimates backscatter noise from topmost range gates."""
+        noise = ma.std(beta[:, -n_gates:], axis=1)
+        noise[noise < noise_min] = noise_min
+        return noise
+
+    @staticmethod
+    def _reset_low_values_above_saturation(beta, is_saturation, saturation_noise):
+        """Removes low values in saturated profiles above peak."""
+        for saturated_profile in np.where(is_saturation)[0]:
+            profile = beta[saturated_profile, :]
+            peak_ind = np.argmax(profile)
+            alt_ind = np.where(profile[peak_ind:] < saturation_noise)[0] + peak_ind
+            beta[saturated_profile, alt_ind] = ma.masked
+        return beta
+
+    def _get_range_squared(self):
+        """Returns range squared (km2)."""
+        return (self.range*M2KM)**2
+
+    @staticmethod
+    def _remove_noise(beta, noise):
+        """Removes points where snr < 5."""
+        snr_limit = 5
+        snr = (beta.T / noise)
+        beta[snr.T < snr_limit] = ma.masked
+        return beta
+
+    @staticmethod
+    def _calc_range_uncorrected_beta(beta, range_squared):
+        return beta / range_squared
+
+    @staticmethod
+    def _calc_range_corrected_beta(beta, range_squared):
+        return beta * range_squared
+
+    def _find_saturated_profiles(self):
+        """Estimates saturated profiles using the variance of the top range gates."""
+        n_gates, var_lim, _, _ = self.noise_params
+        var = np.var(self.backscatter[:, -n_gates:], axis=1)
+        return var < var_lim
+
+    def _calc_sigma_units(self):
+        """Calculates Gaussian peak std parameters."""
+        sigma_minutes = 2
+        sigma_metres = 5
+        time_step = utils.mdiff(self.time) * MINUTES_IN_HOUR
+        alt_step = utils.mdiff(self.range)
+        x = sigma_minutes / time_step
+        y = sigma_metres / alt_step
+        return x, y
 
 
 class JenoptikCeilo(Ceilometer):
@@ -333,7 +434,7 @@ def ceilo2nc(input_file, output_file, location='unknown', altitude=0):
     """Converts Vaisala ceilometer txt-file to netCDF."""
     ceilo = _initialize_ceilo(input_file)
     ceilo.read_ceilometer_file()
-    beta_variants = calc_beta(ceilo)
+    beta_variants = ceilo.calc_beta()
     _append_data(ceilo, beta_variants)
     _append_height(ceilo, altitude)
     output.update_attributes(ceilo.data, ATTRIBUTES)
@@ -376,110 +477,6 @@ def _save_ceilo(ceilo, output_file, location):
     rootgrp.history = f"{utils.get_time()} - ceilometer file created"
     rootgrp.source = ceilo.model
     rootgrp.close()
-
-
-def calc_beta(ceilo):
-    """Converts range-corrected raw beta to beta."""
-
-    def _screen_beta(beta_in, smooth):
-        beta_in = _calc_range_uncorrected_beta(beta_in, range_squared)
-        beta_in = _screen_by_snr(beta_in, ceilo, is_saturation, smooth=smooth)
-        return _calc_range_corrected_beta(beta_in, range_squared)
-
-    range_squared = _get_range_squared(ceilo)
-    is_saturation = _find_saturated_profiles(ceilo)
-    beta = _screen_beta(ceilo.backscatter, False)
-    # smoothed version:
-    beta_smooth = ma.copy(ceilo.backscatter)
-    cloud_ind, cloud_values, cloud_limit = _estimate_clouds_from_beta(beta)
-    beta_smooth[cloud_ind] = cloud_limit  # to reduce artifacts between smoothed and not-smoothed values
-    sigma = _calc_sigma_units(ceilo)
-    beta_smooth = scipy.ndimage.filters.gaussian_filter(beta_smooth, sigma)
-    beta_smooth[cloud_ind] = cloud_values
-    beta_smooth = _screen_beta(beta_smooth, True)
-    return ceilo.backscatter, beta, beta_smooth
-
-
-def _estimate_clouds_from_beta(beta):
-    """Naively finds strong clouds from ceilometer backscatter."""
-    cloud_limit = 1e-6
-    cloud_ind = np.where(beta > cloud_limit)
-    return cloud_ind, beta[cloud_ind], cloud_limit
-
-
-def _screen_by_snr(beta_uncorrected, ceilo, is_saturation, smooth=False):
-    """Screens noise from ceilometer backscatter.
-
-    Args:
-        beta_uncorrected (ndarray): Range-uncorrected backscatter.
-        ceilo (obj): Ceilometer object.
-        is_saturation (ndarray): Boolean array denoting saturated profiles.
-        smooth (bool): Should be true if input beta is smoothed. Default is False.
-
-    """
-    beta = ma.copy(beta_uncorrected)
-    n_gates, _, saturation_noise, noise_min = ceilo.noise_params
-    noise_min = noise_min[0] if smooth else noise_min[1]
-    noise = _estimate_noise_from_top_gates(beta, n_gates, noise_min)
-    beta = _reset_low_values_above_saturation(beta, is_saturation, saturation_noise)
-    beta = _remove_noise(beta, noise)
-    return beta
-
-
-def _estimate_noise_from_top_gates(beta, n_gates, noise_min):
-    """Estimates backscatter noise from topmost range gates."""
-    noise = ma.std(beta[:, -n_gates:], axis=1)
-    noise[noise < noise_min] = noise_min
-    return noise
-
-
-def _reset_low_values_above_saturation(beta, is_saturation, saturation_noise):
-    """Removes low values in saturated profiles above peak."""
-    for saturated_profile in np.where(is_saturation)[0]:
-        profile = beta[saturated_profile, :]
-        peak_ind = np.argmax(profile)
-        alt_ind = np.where(profile[peak_ind:] < saturation_noise)[0] + peak_ind
-        beta[saturated_profile, alt_ind] = ma.masked
-    return beta
-
-
-def _remove_noise(beta, noise):
-    """Removes points where snr < 5."""
-    snr_limit = 5
-    snr = (beta.T / noise)
-    beta[snr.T < snr_limit] = ma.masked
-    return beta
-
-
-def _get_range_squared(ceilo):
-    """Returns range squared (km2)."""
-    return (ceilo.range*M2KM)**2
-
-
-def _calc_range_uncorrected_beta(beta, range_squared):
-    return beta / range_squared
-
-
-def _calc_range_corrected_beta(beta, range_squared):
-    return beta * range_squared
-
-
-def _find_saturated_profiles(ceilo):
-    """Estimates saturated profiles using the variance of the top range gates."""
-    n_gates, var_lim, _, _ = ceilo.noise_params
-    var = np.var(ceilo.backscatter[:, -n_gates:], axis=1)
-    return var < var_lim
-
-
-def _calc_sigma_units(ceilo):
-    """Calculates Gaussian peak std parameters."""
-    sigma_minutes = 2
-    sigma_metres = 5
-    time_step = utils.mdiff(ceilo.time) * MINUTES_IN_HOUR
-    alt_step = utils.mdiff(ceilo.range)
-    x = sigma_minutes / time_step
-    y = sigma_metres / alt_step
-    return x, y
 
 
 def _values_to_dict(keys, values):
