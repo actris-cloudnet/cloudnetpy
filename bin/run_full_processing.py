220230c0a645422930a8c27b103a01da72579fd3
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-06-04 12:26:20 +0300

    processing script update

--- bin/run_full_processing.py
+++ bin/run_full_processing.py
@@ -1,4 +1,11 @@
-""" Run full Cloudnet processing chain.
+"""
+Example script demonstrating full Cloudnet processing for one day.
+
+Measurement files used in this script can be downloaded from:
+
+https://drive.google.com/open?id=1iJVvgDrQo8JvGSOcxroIZc6x8fSUs4EU
+
+
 """
 from cloudnetpy import categorize
 from cloudnetpy.products import iwc, lwc, classification, drizzle
@@ -6,11 +13,11 @@ from cloudnetpy.instruments import mira, ceilo
 from cloudnetpy import plotting as plot
 
 
-FILE_PATH = '/home/tukiains/Documents/PYTHON/cloudnetpy/test_data/'
+FILE_PATH = '/home/tukiains/Documents/PYTHON/cloudnetpy/test_data/mace/'
 
 
 def _get_meta(name):
-    # This cloud come from database OR user can set by hand if needed.
+    # This could come via http API, or user could set by hand if needed.
     return {'name': 'Mace-Head',
             'altitude': 5,
             'institute': 'Finnish Meteorological Institute'}
@@ -26,16 +33,15 @@ def main(site):
     # Radar processing
     # ----------------
 
-    input_file = f"{FILE_PATH}{date}_mace-head_mira_raw.nc"
-    radar_file = f"{FILE_PATH}mira_test_file.nc"
+    input_file = f"{FILE_PATH}{date}_{site}_mira_raw.nc"
+    radar_file = f"{FILE_PATH}radar_file.nc"
     mira.mira2nc(input_file, radar_file, site_meta)
 
     # Ceilometer processing
     # ---------------------
 
-    input_file = f"{FILE_PATH}{date}_MaceHead_CHM070045.nc"
-    ceilo_file = f"{FILE_PATH}ceilo_test_file.nc"
-    cloudnet_ceilo_file = f"{FILE_PATH}{date}_mace-head_chm15k.nc"
+    input_file = f"{FILE_PATH}{date}_{site}_chm15k.nc"
+    ceilo_file = f"{FILE_PATH}ceilo_file.nc"
     ceilo.ceilo2nc(input_file, ceilo_file, site_meta)
 
     # Categorize file
@@ -44,25 +50,25 @@ def main(site):
     categorize_input_files = {
         'radar': radar_file,
         'lidar': ceilo_file,
-        'model': f"{FILE_PATH}20181204_mace-head_ecmwf.nc",
-        'mwr': f"{FILE_PATH}20181204_mace-head_hatpro.nc"
+        'model': f"{FILE_PATH}{date}_{site}_ecmwf.nc",
+        'mwr': f"{FILE_PATH}{date}_{site}_hatpro.nc"
         }
-    categorize_file = f"{FILE_PATH}categorize_test_file.nc"
+    categorize_file = f"{FILE_PATH}categorize_file.nc"
     categorize.generate_categorize(categorize_input_files, categorize_file)
 
     # Products
     # --------
 
-    classification_file = f"{FILE_PATH}classification_test_file.nc"
+    classification_file = f"{FILE_PATH}classification_file.nc"
     classification.generate_class(categorize_file, classification_file)
 
-    iwc_file = f"{FILE_PATH}iwc_test_file.nc"
+    iwc_file = f"{FILE_PATH}iwc_file.nc"
     iwc.generate_iwc(categorize_file, iwc_file)
 
-    lwc_file = f"{FILE_PATH}lwc_test_file.nc"
+    lwc_file = f"{FILE_PATH}lwc_file.nc"
     lwc.generate_lwc(categorize_file, lwc_file)
 
-    drizzle_file = f"{FILE_PATH}drizzle_test_file.nc"
+    drizzle_file = f"{FILE_PATH}drizzle_file.nc"
     drizzle.generate_drizzle(categorize_file, drizzle_file)
 
     # Figures
@@ -76,4 +82,4 @@ def main(site):
 
 
 if __name__ == "__main__":
-    main('mace_head')
+    main('mace-head')
