574935551997a0c173c2652e58970a70cf6dd359
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-06-04 13:06:04 +0300

    loop for product generation

--- bin/run_full_processing.py
+++ bin/run_full_processing.py
@@ -7,8 +7,8 @@ https://drive.google.com/open?id=1iJVvgDrQo8JvGSOcxroIZc6x8fSUs4EU
 
 
 """
+import importlib
 from cloudnetpy import categorize
-from cloudnetpy.products import iwc, lwc, classification, drizzle
 from cloudnetpy.instruments import mira, ceilo
 from cloudnetpy import plotting as plot
 
@@ -59,26 +59,26 @@ def main(site):
     # Products
     # --------
 
-    classification_file = f"{FILE_PATH}classification_file.nc"
-    classification.generate_class(categorize_file, classification_file)
-
-    iwc_file = f"{FILE_PATH}iwc_file.nc"
-    iwc.generate_iwc(categorize_file, iwc_file)
-
-    lwc_file = f"{FILE_PATH}lwc_file.nc"
-    lwc.generate_lwc(categorize_file, lwc_file)
-
-    drizzle_file = f"{FILE_PATH}drizzle_file.nc"
-    drizzle.generate_drizzle(categorize_file, drizzle_file)
+    for product in ('classification', 'iwc', 'lwc', 'drizzle'):
+        product_file = f"{FILE_PATH}{product}_file.nc"
+        module = importlib.import_module(f"cloudnetpy.products.{product}")
+        getattr(module, f"generate_{product}")(categorize_file, product_file)
 
     # Figures
     # -------
 
-    plot.generate_figure(categorize_file, ['Z', 'Z_error', 'ldr', 'v', 'width'])
-    plot.generate_figure(classification_file, ['target_classification', 'detection_status'])
-    plot.generate_figure(iwc_file, ['iwc', 'iwc_error', 'iwc_retrieval_status'])
-    plot.generate_figure(lwc_file, ['lwc', 'lwc_error', 'lwc_retrieval_status'], max_y=6)
-    plot.generate_figure(drizzle_file, ['Do', 'mu', 'S'], max_y=3)
+    plot.generate_figure(categorize_file,
+                         ['Z', 'Z_error', 'ldr', 'v', 'width'])
+    plot.generate_figure(f"{FILE_PATH}classification_file.nc",
+                         ['target_classification', 'detection_status'])
+    plot.generate_figure(f"{FILE_PATH}iwc_file.nc",
+                         ['iwc', 'iwc_error', 'iwc_retrieval_status'])
+    plot.generate_figure(f"{FILE_PATH}lwc_file.nc",
+                         ['lwc', 'lwc_error', 'lwc_retrieval_status'],
+                         max_y=6)
+    plot.generate_figure(f"{FILE_PATH}drizzle_file.nc",
+                         ['Do', 'mu', 'S'],
+                         max_y=3)
 
 
 if __name__ == "__main__":
