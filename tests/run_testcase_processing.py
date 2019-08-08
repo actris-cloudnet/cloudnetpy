cd1940c8e72dad0760de2f32e530fff11dcdc093
Anniina Korpinen <anniina.korpinen@fmi.fi>
2019-08-08 11:06:42 +0300

    Parsing cloudnetpy run to pieces

--- tests/run_testcase_processing.py
+++ tests/run_testcase_processing.py
@@ -13,25 +13,23 @@ from cloudnetpy.instruments import mira, ceilo
 from cloudnetpy import plotting as plot
 
 
-#FILE_PATH = '/home/tukiains/Documents/PYTHON/cloudnetpy/test_data/mace/'
-
-
-def _get_meta(name):
-    # This could come via http API, or user could set by hand if needed.
-    return {'name': name.capitalize(),
-            'altitude': 16,
-            'institute': 'Finnish Meteorological Institute'}
-
-
-def process_cloudnetpy(site, file_path):
-    """ Main function. """
+def process_cloudnetpy_raw_files(site, file_path):
 
     def _process_raw(input_id, output_id, fun):
         print(f"Processing raw {output_id} file...")
-        input_file = _input_file_name(input_id)
+        input_file = _input_file_name(file_path, date, site, input_id)
         output_file = _output_file_name(output_id)
         fun(input_file, output_file, site_meta)
         return output_file
+
+    date = '20181204'
+    site_meta = _get_meta(site)
+
+    _process_raw('mira_raw', 'radar', mira.mira2nc)
+    _process_raw('chm15k_raw', 'ceilo', ceilo.ceilo2nc)
+
+
+def process_cloudnetpy_categorize(site, file_path):
 
     def _process_categorize():
         print(f"Processing categorize file...")
@@ -40,31 +38,43 @@ def process_cloudnetpy(site, file_path):
         categorize.generate_categorize(input_files, output_file)
         return output_file
 
-    def _process_product(product_name):
-        print(f"Processing {product_name} file...")
-        output_file = _output_file_name(product_name)
-        module = importlib.import_module(f"cloudnetpy.products.{product_name}")
-        getattr(module, f"generate_{product_name}")(categorize_file, output_file)
-
     def _get_categorize_input_files():
         return {'radar': radar_file,
                 'lidar': ceilo_file,
                 'model': _input_file_name('ecmwf'),
                 'mwr': _input_file_name('hatpro')}
 
-    def _input_file_name(file_id):
-        return f"{file_path}{date}_{site}_{file_id}.nc"
+    date = '20181204'
+    site_meta = _get_meta(site)
 
-    def _output_file_name(file_id):
-        return f"{file_path}{file_id}_file.nc"
+    _process_categorize()
+
+
+def process_cloudnetpy_products(site):
+
+    def _process_product(product_name):
+        print(f"Processing {product_name} file...")
+        output_file = _output_file_name(product_name)
+        module = importlib.import_module(f"cloudnetpy.products.{product_name}")
+        getattr(module, f"generate_{product_name}")(categorize_file, output_file)
 
     date = '20181204'
     site_meta = _get_meta(site)
 
-    radar_file = _process_raw('mira_raw', 'radar', mira.mira2nc)
-    ceilo_file = _process_raw('chm15k_raw', 'ceilo', ceilo.ceilo2nc)
-
-    categorize_file = _process_categorize()
-
     for product in ('classification', 'iwc', 'lwc', 'drizzle'):
         _process_product(product)
+
+
+def _get_meta(name):
+    return {'name': name.capitalize(),
+            'altitude': 16,
+            'institute': 'Finnish Meteorological Institute'}
+
+
+def _input_file_name(file_path, date, site, file_id):
+    return f"{file_path}{date}_{site}_{file_id}.nc"
+
+
+def _output_file_name(file_path, file_id):
+    return f"{file_path}{file_id}_file.nc"
+
