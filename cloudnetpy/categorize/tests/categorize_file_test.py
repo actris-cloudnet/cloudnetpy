2e9614a3b64c837d26cc0da566c41a0f404e0eeb
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-08-13 16:52:48 +0300

    clean this also

--- cloudnetpy/categorize/tests/categorize_file_test.py
+++ cloudnetpy/categorize/tests/categorize_file_test.py
@@ -1,31 +1,23 @@
 import pytest
 import netCDF4
-from tests.test import initialize_test_data
-import warnings
+from tests.test import initialize_test_data, missing_var_msg
 
-warnings.filterwarnings("ignore")
-
-instrument_list = ['categorize']
-test_data_path = initialize_test_data(instrument_list)
+INSTRUMENT_LIST = ['categorize']
+TEST_DATA_PATH = initialize_test_data(INSTRUMENT_LIST)
 
 
 @pytest.fixture
 def test_data():
     key_dict = {}
-    for i, instrument in enumerate(instrument_list):
-        keys = list(netCDF4.Dataset(test_data_path[i]).variables.keys())
-        key_dict[instrument] = keys
+    for path, instrument in zip(TEST_DATA_PATH, INSTRUMENT_LIST):
+        key_dict[instrument] = set(netCDF4.Dataset(path).variables.keys())
     return key_dict
 
 
 def test_categorize_file(test_data):
-    must_keys = ['v', 'width', 'ldr', 'Z', 'v_sigma', 'Z_error', 'Z_sensitivity',
+    must_keys = {'v', 'width', 'ldr', 'Z', 'v_sigma', 'Z_error', 'Z_sensitivity',
                  'Z_bias', 'latitude', 'longitude', 'altitude', 'time', 'height',
                  'radar_frequency', 'category_bits', 'insect_prob', 'is_rain',
-                 'quality_bits', 'beta', 'model_time', 'temperature', 'q']
-    compared_list = list(set(must_keys).intersection(test_data['categorize']))
-    try:
-        assert set(must_keys) == set(compared_list)
-    except AssertionError:
-        missing_variables = list(set(must_keys).difference(compared_list))
-        raise AssertionError(f"Missing '{', '.join(missing_variables)}' variables in categorize file")
+                 'quality_bits', 'beta', 'model_time', 'temperature', 'q'}
+    missing_keys = must_keys - test_data['categorize']
+    assert not missing_keys, missing_var_msg(missing_keys, 'categorize')
