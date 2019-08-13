907d2961691e4b5754f47b2ff2a14c7b43c74b5c
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-08-13 16:18:21 +0300

    simpler assertionerror raising mechanism when missing required variables in raw files

--- cloudnetpy/instruments/tests/raw_files_test.py
+++ cloudnetpy/instruments/tests/raw_files_test.py
@@ -1,64 +1,49 @@
-import sys
 import pytest
 import netCDF4
 from tests.test import initialize_test_data
-import warnings
 
-warnings.filterwarnings("ignore")
-
-instrument_list = ['mira_raw', 'chm15k_raw', 'hatpro', 'ecmwf']
-test_data_path = initialize_test_data(instrument_list)
+INSTRUMENT_LIST = ['mira_raw', 'chm15k_raw', 'hatpro', 'ecmwf']
+TEST_DATA_PATH = initialize_test_data(INSTRUMENT_LIST)
 
 
 @pytest.fixture
 def test_data():
     key_dict = {}
-    for i, instrument in enumerate(instrument_list):
-        keys = list(netCDF4.Dataset(test_data_path[i]).variables.keys())
-        key_dict[instrument] = keys
+    for i, instrument in enumerate(INSTRUMENT_LIST):
+        keys = list(netCDF4.Dataset(TEST_DATA_PATH[i]).variables.keys())
+        key_dict[instrument] = set(keys)
     return key_dict
 
 
+def _error_msg(missing_keys, name):
+    return f"Variable(s) {missing_keys} missing in {name} file!"
+
+
 def test_mira_raw_file(test_data):
-    must_keys = ['nfft', 'prf', 'nave', 'zrg', 'rg0', 'drg', 'NyquistVelocity',
-                 'time', 'range', 'Zg', 'VELg', 'RMSg', 'LDRg', 'SNRg']
-    compared_list = list(set(must_keys).intersection(test_data['mira_raw']))
-    try:
-        assert set(must_keys) == set(compared_list)
-    except AssertionError:
-        missing_variables = list(set(must_keys).difference(compared_list))
-        raise AssertionError(f"Missing '{', '.join(missing_variables)}' variables in raw Radar file")
+    must_keys = {'nfft', 'prf', 'nave', 'zrg', 'rg0', 'drg', 'NyquistVelocity',
+                 'time', 'range', 'Zg', 'VELg', 'RMSg', 'LDRg', 'SNRg'}
+    missing_keys = must_keys - test_data['mira_raw']
+    assert not missing_keys, _error_msg(missing_keys, 'raw mira')
+
 
 def test_ceilo_raw_file(test_data):
-    must_keys = ['latitude', 'longitude', 'azimuth', 'zenith', 'time', 'range',
-                 'layer', 'altitude', 'wavelength', 'laser_pulses', 'beta_raw']
-    compared_list = list(set(must_keys).intersection(test_data['chm15k_raw']))
-    try:
-        assert set(must_keys) == set(compared_list)
-    except AssertionError:
-        missing_variables = list(set(must_keys).difference(compared_list))
-        raise AssertionError(f"Missing '{', '.join(missing_variables)}' variables in raw Lidar file")
+    must_keys = {'latitude', 'longitude', 'azimuth', 'zenith', 'time', 'range',
+                 'layer', 'altitude', 'wavelength', 'laser_pulses', 'beta_raw'}
+    missing_keys = must_keys - test_data['chm15k_raw']
+    assert not missing_keys, _error_msg(missing_keys, 'raw lidar')
 
 
 def test_model_file(test_data):
-    must_keys = ['latitude', 'longitude', 'horizontal_resolution', 'time', 'forecast_time',
+    must_keys = {'latitude', 'longitude', 'horizontal_resolution', 'time', 'forecast_time',
                  'level', 'pressure', 'uwind', 'vwind', 'omega', 'temperature',
-                 'q', 'rh']
-    compared_list = list(set(must_keys).intersection(test_data['ecmwf']))
-    try:
-        assert set(must_keys) == set(compared_list)
-    except AssertionError:
-        missing_variables = list(set(must_keys).difference(compared_list))
-        raise AssertionError(f"Missing '{', '.join(missing_variables)}' variables in Model file")
+                 'q', 'rh'}
+    missing_keys = must_keys - test_data['ecmwf']
+    assert not missing_keys, _error_msg(missing_keys, 'ecmwf model')
 
 
 def test_mwr_file(test_data):
-    must_keys = ['time_reference', 'minimum', 'maximum', 'time', 'rain_flag',
-                 'elevation_angle','azimuth_angle', 'retrieval', 'LWP_data']
-    compared_list = list(set(must_keys).intersection(test_data['hatpro']))
-    try:
-        assert set(must_keys) == set(compared_list)
-    except AssertionError:
-        missing_variables = list(set(must_keys).difference(compared_list))
-        raise AssertionError(f"Missing '{', '.join(missing_variables)}' variables in Microwave radiometer file")
+    must_keys = {'time_reference', 'minimum', 'maximum', 'time', 'rain_flag',
+                 'elevation_angle', 'azimuth_angle', 'retrieval', 'LWP_data'}
 
+    missing_keys = must_keys - test_data['hatpro']
+    assert not missing_keys, _error_msg(missing_keys, 'hatpro')
