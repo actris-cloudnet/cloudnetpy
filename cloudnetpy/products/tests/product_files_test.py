8149687fe8ffc569f77156c869ea5e3d8f2f32d8
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-08-13 17:29:24 +0300

    loop over different tests makes it cleaner

--- cloudnetpy/products/tests/product_files_test.py
+++ cloudnetpy/products/tests/product_files_test.py
@@ -1,33 +1,24 @@
 from tests.test import collect_variables, missing_var_msg
 
-INSTRUMENT_LIST = ['classification', 'iwc', 'lwc', 'drizzle']
-TEST_DATA = collect_variables(INSTRUMENT_LIST)
+MUST_BE_KEYS = {
+    'classification':
+        {'target_classification', 'detection_status'},
+    'iwc':
+        {'iwc', 'iwc_inc_rain', 'iwc_bias', 'iwc_error', 'iwc_sensitivity',
+         'iwc_retrieval_status'},
+    'lwc':
+        {'lwc', 'lwc_error', 'lwc_retrieval_status', 'lwp', 'lwp_error'},
+    'drizzle':
+        {'Do', 'mu', 'S', 'beta_corr', 'drizzle_N', 'drizzle_lwc', 'drizzle_lwf',
+         'v_drizzle', 'v_air', 'Do_error', 'drizzle_lwc_error',
+         'drizzle_lwf_error', 'S_error', 'Do_bias', 'drizzle_lwc_bias',
+         'drizzle_lwf_bias', 'drizzle_N_error', 'v_drizzle_error',
+         'drizzle_N_bias', 'v_drizzle_bias', 'drizzle_retrieval_status'}
+    }
 
 
-def test_classification_file():
-    must_keys = {'target_classification', 'detection_status'}
-    missing_keys = must_keys - TEST_DATA['classification']
-    assert not missing_keys, missing_var_msg(missing_keys, 'classification')
-
-
-def test_iwc_file():
-    must_keys = {'iwc', 'iwc_inc_rain', 'iwc_bias', 'iwc_error', 'iwc_sensitivity',
-                 'iwc_retrieval_status'}
-    missing_keys = must_keys - TEST_DATA['iwc']
-    assert not missing_keys, missing_var_msg(missing_keys, 'iwc')
-
-
-def test_lwc_file():
-    must_keys = {'lwc', 'lwc_error', 'lwc_retrieval_status', 'lwp', 'lwp_error'}
-    missing_keys = must_keys - TEST_DATA['lwc']
-    assert not missing_keys, missing_var_msg(missing_keys, 'lwc')
-
-
-def test_drizzle_file():
-    must_keys = {'Do', 'mu', 'S', 'beta_corr', 'drizzle_N', 'drizzle_lwc', 'drizzle_lwf',
-                 'v_drizzle', 'v_air', 'Do_error', 'drizzle_lwc_error',
-                 'drizzle_lwf_error', 'S_error', 'Do_bias', 'drizzle_lwc_bias',
-                 'drizzle_lwf_bias', 'drizzle_N_error', 'v_drizzle_error',
-                 'drizzle_N_bias', 'v_drizzle_bias', 'drizzle_retrieval_status'}
-    missing_keys = must_keys - TEST_DATA['drizzle']
-    assert not missing_keys, missing_var_msg(missing_keys, 'drizzle')
+def test_all():
+    test_data = collect_variables(MUST_BE_KEYS.keys())
+    for key in MUST_BE_KEYS:
+        missing_keys = MUST_BE_KEYS[key] - test_data[key]
+        assert not missing_keys, missing_var_msg(missing_keys, key)
