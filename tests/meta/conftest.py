4f49089dbe81265e4e9a86127e07c43d5d206330
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-10-13 10:34:01 +0300

    unit check as function

--- tests/meta/conftest.py
+++ tests/meta/conftest.py
@@ -29,29 +29,21 @@ def variable(pytestconfig):
 
 class GlobalAttribute:
     def __init__(self, file):
-        self._nc = netCDF4.Dataset(file)
-        self._keys = self._nc.ncattrs()
-        self.bad_units = self._check_attr_units()
-        self.bad_values = self._check_attr_limits()
-        self._nc.close()
+        self.file_name = file
+        self.bad_values = self._check_limits()
+        self.bad_units = _check_units('attributes_units', file)
 
-    def _check_attr_units(self):
+    def _check_limits(self):
         bad = {}
-        for attr, reference in CONFIG.items('attributes_units'):
-            if attr in self._keys:
-                value = self._nc.getncattr(attr)
-                if reference != value:
-                    bad[attr] = value
-        return bad
-
-    def _check_attr_limits(self):
-        bad = {}
+        nc = netCDF4.Dataset(self.file_name)
+        keys = nc.ncattrs()
         for attr, limits in CONFIG.items('attributes_limits'):
-            if attr in self._keys:
+            if attr in keys:
                 limits = tuple(map(float, limits.split(',')))
-                value = int(self._nc.getncattr(attr))
+                value = int(nc.getncattr(attr))
                 if value < limits[0] or value > limits[1]:
                     bad[attr] = value
+        nc.close()
         return bad
 
 
@@ -59,16 +51,17 @@ class Variable:
     def __init__(self, file):
         self.file_name = file
         self.bad_values = check_var_limits(CONFIG, 'variables_limits', file)
-        self.bad_units = self._check_units()
+        self.bad_units = _check_units('variables_units', file)
 
-    def _check_units(self):
-        bad = {}
-        nc = netCDF4.Dataset(self.file_name)
-        keys = nc.variables.keys()
-        for var, reference in CONFIG.items('variables_units'):
-            if var in keys:
-                value = nc.variables[var].units
-                if reference != value:
-                    bad[var] = value
-        nc.close()
-        return bad
+
+def _check_units(config_field, file_name):
+    bad = {}
+    nc = netCDF4.Dataset(file_name)
+    keys = nc.ncattrs() if 'attributes' in config_field else nc.variables.keys()
+    for var, reference in CONFIG.items(config_field):
+        if var in keys:
+            value = nc.variables[var].units
+            if reference != value:
+                bad[var] = value
+    nc.close()
+    return bad
