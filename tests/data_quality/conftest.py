e5578bbed91df911c456bca5d1f92fcf33f95032
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-10-14 10:25:27 +0300

    testing tuning

--- tests/data_quality/conftest.py
+++ tests/data_quality/conftest.py
@@ -5,12 +5,6 @@ CONFIG = utils.read_config('data_quality/data_quality_config.ini')
 
 
 @pytest.fixture
-def missing_variables(pytestconfig):
-    file_name = pytestconfig.option.test_file
-    return utils.find_missing_keys(CONFIG, 'quantities', file_name)
-
-
-@pytest.fixture
 def data(pytestconfig):
     file = pytestconfig.option.test_file
     return InputData(file)
--- tests/data_quality/test_data.py
+++ tests/data_quality/test_data.py
@@ -1,7 +1,3 @@
 
-def test_variable_keys(missing_variables):
-    assert not missing_variables
-
-
 def test_variable_values(data):
     assert not data.bad_values
