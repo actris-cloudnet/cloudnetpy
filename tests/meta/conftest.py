f925ded4ecf7597ea966e516583ed7f5da0f9803
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-10-15 17:22:44 +0300

    classes to functions

--- tests/meta/conftest.py
+++ tests/meta/conftest.py
@@ -7,6 +7,35 @@ CONFIG = utils.read_config('meta/metadata_config.ini')
 
 
 @pytest.fixture
+def invalid_global_attribute_values(pytestconfig):
+    bad = {}
+    nc = netCDF4.Dataset(pytestconfig.option.test_file)
+    nc_keys = nc.ncattrs()
+    for attr, limits in CONFIG.items('attribute_limits'):
+        if attr in nc_keys:
+            limits = tuple(map(float, limits.split(',')))
+            value = int(nc.getncattr(attr))
+            if not limits[0] <= value <= limits[1]:
+                bad[attr] = value
+    nc.close()
+    return bad
+
+
+@pytest.fixture
+def invalid_variable_units(pytestconfig):
+    bad = {}
+    nc = netCDF4.Dataset(pytestconfig.option.test_file)
+    nc_keys = nc.variables.keys()
+    for var, reference in CONFIG.items('variable_units'):
+        if var in nc_keys:
+            value = nc.variables[var].units
+            if reference != value:
+                bad[var] = value
+    nc.close()
+    return bad
+
+
+@pytest.fixture
 def missing_variables(pytestconfig):
     file_name = pytestconfig.option.test_file
     return _find_missing_keys('required_variables', file_name)
@@ -16,53 +45,6 @@ def missing_variables(pytestconfig):
 def missing_global_attributes(pytestconfig):
     file_name = pytestconfig.option.test_file
     return _find_missing_keys('required_global_attributes', file_name)
-
-
-@pytest.fixture
-def global_attribute(pytestconfig):
-    return GlobalAttribute(pytestconfig.option.test_file)
-
-
-@pytest.fixture
-def variable(pytestconfig):
-    return Variable(pytestconfig.option.test_file)
-
-
-class GlobalAttribute:
-    def __init__(self, file):
-        self.file_name = file
-        self.bad_values = self._check_values()
-
-    def _check_values(self):
-        bad = {}
-        nc = netCDF4.Dataset(self.file_name)
-        nc_keys = nc.ncattrs()
-        for attr, limits in CONFIG.items('attribute_limits'):
-            if attr in nc_keys:
-                limits = tuple(map(float, limits.split(',')))
-                value = int(nc.getncattr(attr))
-                if not limits[0] <= value <= limits[1]:
-                    bad[attr] = value
-        nc.close()
-        return bad
-
-
-class Variable:
-    def __init__(self, file):
-        self.file_name = file
-        self.bad_units = self._check_units()
-
-    def _check_units(self):
-        bad = {}
-        nc = netCDF4.Dataset(self.file_name)
-        nc_keys = nc.variables.keys()
-        for var, reference in CONFIG.items('variable_units'):
-            if var in nc_keys:
-                value = nc.variables[var].units
-                if reference != value:
-                    bad[var] = value
-        nc.close()
-        return bad
 
 
 def _find_missing_keys(config_field, file_name):
--- tests/meta/test_metadata.py
+++ tests/meta/test_metadata.py
@@ -1,17 +1,17 @@
 # Test functions for metadata testing
 
 
-def test_variable_keys(missing_variables):
+def test_variables(missing_variables):
     assert not missing_variables
 
 
-def test_variable_units(variable):
-    assert not variable.bad_units
-
-
-def test_attribute_keys(missing_global_attributes):
+def test_global_attributes(missing_global_attributes):
     assert not missing_global_attributes
 
 
-def test_attribute_values(global_attribute):
-    assert not global_attribute.bad_values
+def test_variable_units(invalid_variable_units):
+    assert not invalid_variable_units
+
+
+def test_attribute_values(invalid_global_attribute_values):
+    assert not invalid_global_attribute_values
