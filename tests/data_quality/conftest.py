267b8f81571a21d3525dd94b6c0b2098bd1e6667
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-10-15 17:10:05 +0300

    separate fixtures better than one big class

--- tests/data_quality/conftest.py
+++ tests/data_quality/conftest.py
@@ -7,26 +7,26 @@ CONFIG = utils.read_config('data_quality/data_quality_config.ini')
 
 
 @pytest.fixture
-def data(pytestconfig):
-    file = pytestconfig.option.test_file
-    return Variable(file)
+def out_of_limits_values(pytestconfig):
+    bad = {}
+    nc = netCDF4.Dataset(pytestconfig.option.test_file)
+    for var, limits in CONFIG.items('limits'):
+        if var in nc.variables.keys():
+            limits = tuple(map(float, limits.split(',')))
+            if np.min(nc.variables[var][:]) < limits[0]:
+                bad[var] = 'min value out of limits'
+            if np.max(nc.variables[var][:]) > limits[1]:
+                bad[var] = 'max value out of limits'
+    nc.close()
+    return bad
 
 
-class Variable:
-    def __init__(self, file):
-        self.file_name = file
-        self.bad_values = self._check_var_limits()
-
-    def _check_var_limits(self):
-        bad = {}
-        nc = netCDF4.Dataset(self.file_name)
-        nc_keys = nc.variables.keys()
-        for var, limits in CONFIG.items('limits'):
-            if var in nc_keys:
-                limits = tuple(map(float, limits.split(',')))
-                min_value = np.min(nc.variables[var][:])
-                max_value = np.max(nc.variables[var][:])
-                if min_value < limits[0] or max_value > limits[1]:
-                    bad[var] = [min_value, max_value]
-        nc.close()
-        return bad
+@pytest.fixture
+def invalid_values(pytestconfig):
+    bad = {}
+    nc = netCDF4.Dataset(pytestconfig.option.test_file)
+    for key in nc.variables.keys():
+        if np.isnan(nc.variables[key][:]).any():
+            bad[key] = 'contains invalid values'
+    nc.close()
+    return bad
--- tests/data_quality/test_data.py
+++ tests/data_quality/test_data.py
@@ -1,3 +1,12 @@
+# Test functions for data quality testing
 
-def test_variable_values(data):
-    assert not data.bad_values
+
+def test_min_max(out_of_limits_values):
+    assert not out_of_limits_values
+
+
+def test_invalid(invalid_values):
+    assert not invalid_values
+
+
+
