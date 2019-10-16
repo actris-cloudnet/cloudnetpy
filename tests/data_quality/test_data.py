f5fc59b5ae8b2275e797f5c0a18d0690be4e1a40
Anniina Korpinen <anniina.korpinen@fmi.fi>
2019-10-16 10:04:40 +0300

    First version of logging writing ready, merged with master

--- tests/data_quality/test_data.py
+++ tests/data_quality/test_data.py
@@ -1,3 +1,13 @@
+from tests.utils import fill_log
+# Test functions for data quality testing
 
-def test_variable_values(data):
-    assert not data.bad_values
+
+def test_min_max(out_of_limits_values):
+    assert not out_of_limits_values,\
+        fill_log("Data not within limits", out_of_limits_values)
+
+
+def test_invalid(invalid_values):
+    assert not invalid_values, \
+        fill_log("Eather NaNs or Inf in data", invalid_values)
+
--- tests/meta/test_metadata.py
+++ tests/meta/test_metadata.py
@@ -1,17 +1,22 @@
+from tests.utils import fill_log
 # Test functions for metadata testing
 
 
 def test_variables(missing_variables):
-    assert not missing_variables
+    assert not missing_variables, \
+        fill_log(f"Variables are missing", missing_variables)
 
 
 def test_global_attributes(missing_global_attributes):
-    assert not missing_global_attributes
+    assert not missing_global_attributes, \
+        fill_log(f"Attributes are missing", missing_global_attributes)
 
 
 def test_variable_units(invalid_variable_units):
-    assert not invalid_variable_units
+    assert not invalid_variable_units, \
+        fill_log(f"Wrong variable units", invalid_variable_units)
 
 
 def test_attribute_values(invalid_global_attribute_values):
-    assert not invalid_global_attribute_values
+    assert not invalid_global_attribute_values, \
+        fill_log(f"Variable values not within the limits", invalid_global_attribute_values)
