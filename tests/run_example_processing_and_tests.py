9555744eb27bb9932a7deaed00ac985f79e89ab0
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-10-21 16:56:41 +0300

    test imports

--- tests/run_example_processing_and_tests.py
+++ tests/run_example_processing_and_tests.py
@@ -8,7 +8,7 @@ import importlib
 from cloudnetpy.instruments.mira import mira2nc
 from cloudnetpy.instruments.ceilo import ceilo2nc
 from cloudnetpy.categorize.categorize import generate_categorize
-from tests.api import check_data_quality, check_metadata, run_unit_tests, utils
+from tests import check_data_quality, check_metadata, run_unit_tests, utils
 
 PROCESS = True
 
