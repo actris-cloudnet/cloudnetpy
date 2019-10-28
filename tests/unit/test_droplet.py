635d44e949fe0e90145155e459d1f3b47626ff9b
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-10-28 12:00:03 +0200

    top ind unit tests

--- tests/unit/test_droplet.py
+++ tests/unit/test_droplet.py
@@ -37,4 +37,41 @@ class TestIndBase:
         dx = ma.diff(mx).filled(0)
         with pytest.raises(IndexError):
             droplet.ind_base(dx, self.peak_position, self.search_range,
-                             self.threshold) == 4
+                             self.threshold)
+
+
+class TestIndTop:
+
+    x = np.array([1, 3, 8, 4, -99, 1, 0.5, 0])
+    mx = ma.masked_array(x, mask=[0, 0, 0, 0, 1, 0, 0, 0])
+    dx = ma.diff(mx).filled(0)
+    peak_position = 2
+    n_prof = x.shape[0]
+    search_range = 5
+    threshold = 2
+
+    def test_ind_top_0(self):
+        assert droplet.ind_top(self.dx, self.peak_position, self.n_prof,
+                               self.search_range, self.threshold) == 3
+
+    def test_ind_top_1(self):
+        search_range = 100
+        assert droplet.ind_top(self.dx, self.peak_position, self.n_prof,
+                               search_range, self.threshold) == 3
+
+    def test_ind_top_2(self):
+        huge_threshold = 5000
+        assert droplet.ind_top(self.dx, self.peak_position, self.n_prof,
+                               self.search_range, huge_threshold) == 7
+
+    def test_ind_top_3(self):
+        small_threshold = 1.01
+        assert droplet.ind_top(self.dx, self.peak_position, self.n_prof,
+                               self.search_range, small_threshold) == 3
+
+    def test_ind_top_4(self):
+        mx = ma.masked_array(self.x, mask=[1, 0, 1, 0, 1, 0, 1, 0])
+        dx = ma.diff(mx).filled(0)
+        with pytest.raises(IndexError):
+            droplet.ind_top(dx, self.peak_position, self.n_prof,
+                            self.search_range, self.threshold)
