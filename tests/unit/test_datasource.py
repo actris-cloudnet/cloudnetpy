64f11e1707f47b6345ba2daf8adba1f800ee8a3f
Anniina Korpinen <anniina.korpinen@fmi.fi>
2019-10-29 09:22:34 +0200

    merged with master

--- tests/unit/test_datasource.py
+++ tests/unit/test_datasource.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy.testing import assert_array_equal
 import cloudnetpy.categorize as ds
+from cloudnetpy.categorize import datasource
+import pytest
 
 
 class FakeData(object):
@@ -26,4 +28,50 @@ def test_m2km():
     res = np.array([10, 20, 30, 40])
     cnet = ds.DataSource.m2km(var)
     assert_array_equal(res, cnet)
+
+
+def test_init_altitude(nc_file):
+    obj = datasource.DataSource(nc_file)
+    assert obj.altitude == 500
+
+
+def test_getvar(nc_file, test_array):
+    obj = datasource.DataSource(nc_file)
+    assert_array_equal(obj.getvar('model_height'), test_array)
+
+
+def test_getvar_missing(nc_file):
+    obj = datasource.DataSource(nc_file)
+    with pytest.raises(RuntimeError):
+        obj.getvar('not_existing_variable')
+
+
+def test_init_time(nc_file, test_array):
+    obj = datasource.DataSource(nc_file)
+    assert_array_equal(obj.time, test_array)
+
+
+class FakeProfileDataSource(datasource.ProfileDataSource):
+    def __init__(self, value, units):
+        self.variables = {'height': Variable(value, units)}
+        self.height = self._get_height()
+
+
+class Variable:
+    def __init__(self, value, units):
+        self.value = value
+        self.units = units
+
+    def __getitem__(self, ind):
+        return self.value[ind]
+
+
+def test_get_height_m():
+    obj = FakeProfileDataSource(np.array([1, 2, 3]), 'km')
+    assert_array_equal(obj.height, [1000, 2000, 3000])
+
+
+def test_get_height_km():
+    obj = FakeProfileDataSource(np.array([1000, 2000, 3000]), 'm')
+    assert_array_equal(obj.height, [1000, 2000, 3000])
 
