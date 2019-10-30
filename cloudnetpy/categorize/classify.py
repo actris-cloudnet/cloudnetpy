8b2ccd5305aa3934cd0ec80c94cb5339dfc47e5d
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-10-30 17:17:23 +0200

    melting module

--- cloudnetpy/categorize/classify.py
+++ cloudnetpy/categorize/classify.py
@@ -9,6 +9,7 @@ from scipy.interpolate import interp1d
 from cloudnetpy import utils
 from cloudnetpy.categorize import droplet, atmos
 from cloudnetpy.constants import T0
+from cloudnetpy.categorize import melting
 
 
 def fetch_quality(radar, lidar, classification, attenuations):
@@ -68,7 +69,7 @@ def classify_measurements(radar, lidar, model, mwr):
     obs = _ClassData(radar, lidar, model, mwr)
     bits = [None] * 6
     liquid = droplet.find_liquid(obs)
-    bits[3] = find_melting_layer(obs)
+    bits[3] = melting.find_melting_layer(obs)
     bits[2] = find_freezing_region(obs, bits[3])
     bits[0] = droplet.correct_liquid_top(obs, liquid, bits[2], limit=500)
     bits[5], insect_prob = find_insects(obs, bits[3], bits[0])
@@ -80,98 +81,6 @@ def classify_measurements(radar, lidar, model, mwr):
                                 insect_prob,
                                 liquid['bases'],
                                 find_profiles_with_undetected_melting(bits))
-
-
-def find_melting_layer(obs, smooth=True):
-    """Finds melting layer from model temperature, ldr, and velocity.
-
-    Melting layer is detected using linear depolarization ratio, *ldr*,
-    Doppler velocity, *v*, and wet-bulb temperature, *Tw*.
-
-    The algorithm is based on *ldr* having a clear Gaussian peak around
-    the melting layer. This signature is caused by the growth of ice
-    crystals into snowflakes that are much larger. In addition, when snow and
-    ice melt, emerging heavy water droplets start to drop rapidly towards
-    ground. Thus, there is also a similar positive peak in the
-    first difference of *v*.
-
-    The peak in *ldr* is the primary parameter we analyze. If
-    *ldr* has a proper peak, and *v* < -1 m/s in the base, melting layer
-    has been found. If *ldr* is missing we only analyze the behaviour
-    of *v*, which is always present, to detect the melting layer.
-
-    Model temperature is used to limit the melting layer search to a certain
-    temperature range around 0 C. For ECMWF the range is -4..+3, and for
-    the rest -8..+6.
-
-    Notes:
-        There might be some detection problems with strong updrafts of air.
-        In these cases the absolute values for speed do not make sense (rain
-        drops can even move upwards instead of down).
-
-    Args:
-        obs (_ClassData): Input data container.
-        smooth (bool, optional): If True, apply a small
-            Gaussian smoother to the melting layer. Default is True.
-
-    Returns:
-        ndarray: 2-D boolean array denoting the melting layer.
-
-    """
-
-    def _slice(arg1, arg2):
-        out1, out2 = arg1[ind, temp_indices], arg2[ind, temp_indices]
-        return out1, out2, ma.count(out1)
-
-    def _basetop(dprof, pind):
-        top1 = droplet.ind_top(dprof, pind, len(temp_indices), 10, 2)
-        base1 = droplet.ind_base(dprof, pind, 10, 2)
-        return top1, base1
-
-    def _get_temp_indices():
-        bottom_point = np.where(t_prof < (T0 - t_range[0]))[0][0]
-        top_point = np.where(t_prof > (T0 + t_range[0]))[0]
-        top_point = top_point[-1] if len(top_point) > 0 else 0
-        return np.arange(bottom_point, top_point + 1)
-
-    if 'ecmwf' in obs.model_type.lower():
-        t_range = (-4, 3)
-    else:
-        t_range = (-8, 6)
-
-    melting_layer = np.zeros(obs.tw.shape, dtype=bool)
-    ldr_diff = np.diff(obs.ldr, axis=1).filled(0)
-    v_diff = np.diff(obs.v, axis=1).filled(0)
-
-    for ind, t_prof in enumerate(obs.tw):
-        temp_indices = _get_temp_indices()
-        ldr_prof, ldr_dprof, nldr = _slice(obs.ldr, ldr_diff)
-        v_prof, v_dprof, nv = _slice(obs.v, v_diff)
-
-        if nldr > 3 or nv > 3:
-            ldr_p = np.argmax(ldr_prof)
-            v_p = np.argmax(v_dprof)
-
-            try:
-                top, base = _basetop(ldr_dprof, ldr_p)
-                conds = (ldr_prof[ldr_p] - ldr_prof[top] > 4,
-                         ldr_prof[ldr_p] - ldr_prof[base] > 4,
-                         ldr_prof[ldr_p] > -30,
-                         v_prof[base] < -1)
-                if all(conds):
-                    melting_layer[ind, temp_indices[ldr_p]:temp_indices[top]+1] = True
-            except:
-                try:
-                    top, base = _basetop(v_dprof, v_p)
-                    diff = v_prof[top] - v_prof[base]
-                    if diff > 0.5 and v_prof[base] < -2:
-                        melting_layer[ind, temp_indices[v_p-1:v_p+2]] = True
-                except:
-                    continue
-    if smooth:
-        smoothed_layer = gaussian_filter(np.array(melting_layer, dtype=float), (2, 0.1))
-        melting_layer = (smoothed_layer > 0.2).astype(bool)
-    return melting_layer
 
 
 def find_freezing_region(obs, melting_layer):
