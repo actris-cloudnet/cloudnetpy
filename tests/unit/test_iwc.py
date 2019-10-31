9b3e92ba990f443a7034793cfdba59749221dc8a
Anniina Korpinen <anniina.korpinen@fmi.fi>
2019-10-31 16:51:45 +0200

    Starting writing unit tests to IWC

---
+++ tests/unit/test_iwc.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+def test_find_ice(cat_file):
+    assert True
+
+
+def test_find_would_be_ice(cat_file):
+    assert True
+
+
+def test_find_corrected_ice(cat_file):
+    assert True
+
+
+def test_find_uncorrected_ice(cat_file):
+    assert True
+
+
+def test_find_ice_above_rain(cat_file):
+    assert True
+
+
+def test_find_cold_above_rain(cat_file):
+    assert True
