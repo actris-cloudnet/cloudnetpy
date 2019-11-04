936f05631099eaeaf910b5c69f61008565ed5303
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-11-04 17:24:47 +0200

    more model tests

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -2,6 +2,7 @@ import numpy as np
 import numpy.ma as ma
 from numpy.testing import assert_array_equal
 import pytest
+import netCDF4
 from cloudnetpy.categorize import model
 
 
@@ -21,3 +22,73 @@ def test_calc_mean_height():
                        [-0.2, 0.8, 1.8, 2.8, 3.8]])
     result = np.array([0, 1, 2, 3, 4])
     assert_array_equal(model._calc_mean_height(height), result)
+
+
+BIG_ARRAY = np.ones((2, 3, 4))
+
+EMPTY_ARRAY = ma.array([[0, 0, 0, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0]],
+                       mask=[[0, 0, 0, 0],
+                             [0, 0, 0, 0],
+                             [0, 0, 0, 0]], dtype=float)
+MASKED_ONE = ma.copy(EMPTY_ARRAY)
+MASKED_ONE[0, 0] = ma.masked
+
+ALT_SITE = 123
+
+
+@pytest.fixture(scope='session')
+def fake_model_file(tmpdir_factory):
+    """Creates a simple radar file for testing."""
+    file_name = tmpdir_factory.mktemp("data").join("ecmwf_file.nc")
+    root_grp = netCDF4.Dataset(file_name, "w", format="NETCDF4_CLASSIC")
+    n_time, n_height = 3, 4
+    root_grp.createDimension('time', n_time)
+    root_grp.createDimension('height', n_height)
+    root_grp.createDimension('wl', 2)
+    root_grp.createVariable('time', 'f8', 'time')[:] = np.arange(n_time)
+    var = root_grp.createVariable('height', 'f8', ('time', 'height'))
+    var[:] = np.array([[1000, 2000, 3000, 4000],
+                       [1000, 2000, 3000, 4000],
+                       [1000, 2000, 3000, 4000]])
+    var.units = 'm'
+    _create_var(root_grp, 'temperature')
+    _create_var(root_grp, 'pressure')
+    _create_var(root_grp, 'uwind')
+    _create_var(root_grp, 'vwind')
+    _create_var(root_grp, 'q')
+    _create_var(root_grp, 'rh')
+    _create_var(root_grp, 'gas_atten', data=BIG_ARRAY, dim=('wl', 'time', 'height'))
+    _create_var(root_grp, 'specific_gas_atten', data=BIG_ARRAY, dim=('wl', 'time', 'height'))
+    _create_var(root_grp, 'specific_saturated_gas_atten', data=BIG_ARRAY, dim=('wl', 'time', 'height'))
+    _create_var(root_grp, 'liquid_atten', data=BIG_ARRAY, dim=('wl', 'time', 'height'))
+    _create_var(root_grp, 'specific_liquid_atten', data=BIG_ARRAY, dim=('wl', 'time', 'height'))
+    root_grp.close()
+    return file_name
+
+
+def _create_var(root_grp, name, dim=('time', 'height'), data=EMPTY_ARRAY):
+    var = root_grp.createVariable(name, 'f8', dim)
+    var[:] = data
+    var.units = 'g'
+
+
+def test_get_model_heights(fake_model_file):
+    obj = model.Model(str(fake_model_file), ALT_SITE)
+    assert_array_equal(obj.model_heights, ALT_SITE + np.array([[1000, 2000, 3000, 4000],
+                                                               [1000, 2000, 3000, 4000],
+                                                               [1000, 2000, 3000, 4000]]))
+
+
+def test_mean_height(fake_model_file):
+    obj = model.Model(str(fake_model_file), ALT_SITE)
+    assert_array_equal(obj.mean_height, ALT_SITE + np.array([1000, 2000, 3000, 4000]))
+
+
+def test_interpolate_to_common_height(fake_model_file):
+    obj = model.Model(str(fake_model_file), ALT_SITE)
+    radar_wl_band = 0
+    obj.interpolate_to_common_height(radar_wl_band)
+    for key in ('uwind', 'vwind', 'q'):
+        assert key in obj.data_sparse
