5381c45ba667bbd85b5e72a91fb7dcd4dbf73f6b
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-11-04 15:14:20 +0200

    modified categorize

--- cloudnetpy/categorize/categorize.py
+++ cloudnetpy/categorize/categorize.py
@@ -7,6 +7,7 @@ from cloudnetpy.categorize import atmos, classify
 from cloudnetpy.metadata import MetaData
 from cloudnetpy.categorize import DataSource, ProfileDataSource
 from cloudnetpy.categorize.radar import Radar
+from cloudnetpy.categorize.model import Model
 
 
 def generate_categorize(input_files, output_file):
@@ -176,111 +177,6 @@ class Mwr(DataSource):
         random_error, bias = 0.25, 50
         lwp_error = utils.l2norm(self.data['lwp'][:]*random_error, bias)
         self.append_data(lwp_error, 'lwp_error', units='g m-2')
-
-
-class Model(DataSource):
-    """Model class, child of DataSource.
-
-    Args:
-        model_file (str): File name of the NWP model file.
-        alt_site (float): Altitude of the site above mean sea level (m).
-
-    Attributes:
-        type (str): Model type, e.g. 'gdas1' or 'ecwmf'.
-        model_heights (ndarray): 2-D array of model heights (one for each time
-            step).
-        mean_height (ndarray): Mean of *model_heights*.
-        data_sparse (dict): Model variables in common height grid but without
-            interpolation in time.
-        data_dense (dict): Model variables interpolated to Cloudnet's dense
-            time / height grid.
-
-    """
-    fields_dense = ('temperature', 'pressure', 'rh',
-                    'gas_atten', 'specific_gas_atten',
-                    'specific_saturated_gas_atten',
-                    'specific_liquid_atten')
-    fields_sparse = fields_dense + ('q', 'uwind', 'vwind')
-
-    def __init__(self, model_file, alt_site):
-        super().__init__(model_file)
-        self.type = self._get_model_type()
-        self.model_heights = self._get_model_heights(alt_site)
-        self.mean_height = self._get_mean_height()
-        self.height = None
-        self.data_sparse = {}
-        self.data_dense = {}
-        self._append_grid()
-
-    def interpolate_to_common_height(self, wl_band):
-        """Interpolates model variables to common height grid.
-
-        Args:
-            wl_band (int): Integer denoting the wavelength band of the
-                radar (0=35.5 GHz, 1=94 GHz).
-
-        """
-        def _interpolate_variable():
-            datai = np.zeros((len(self.time), len(self.mean_height)))
-            for ind, (alt, prof) in enumerate(zip(self.model_heights, data)):
-                fun = interp1d(alt, prof, fill_value='extrapolate')
-                datai[ind, :] = fun(self.mean_height)
-            return CloudnetArray(datai, key, units)
-
-        for key in self.fields_sparse:
-            variable = self.dataset.variables[key]
-            data = np.array(variable[:])
-            units = variable.units
-            if 'atten' in key:
-                data = data[wl_band, :, :]
-            self.data_sparse[key] = _interpolate_variable()
-
-    def interpolate_to_grid(self, time_grid, height_grid):
-        """Interpolates model variables to Cloudnet's dense time / height grid.
-
-        Args:
-            time_grid (ndarray): The target time array (fraction hour).
-            height_grid (ndarray): The target height array (m).
-
-        """
-        for key in self.fields_dense:
-            self.data_dense[key] = utils.interpolate_2d(self.time,
-                                                        self.mean_height,
-                                                        self.data_sparse[key][:],
-                                                        time_grid, height_grid)
-        self.height = height_grid
-
-    def calc_wet_bulb(self):
-        """Calculates wet-bulb temperature in dense grid."""
-        wet_bulb_temp = atmos.calc_wet_bulb_temperature(self.data_dense)
-        self.append_data(wet_bulb_temp, 'Tw', units='K')
-
-    def screen_sparse_fields(self):
-        """Removes model fields that we don't want to write in the output."""
-        fields_to_keep = ('temperature', 'pressure', 'q', 'uwind', 'vwind')
-        self.data_sparse = {key: self.data_sparse[key]
-                            for key in fields_to_keep}
-
-    def _append_grid(self):
-        self.append_data(self.time, 'model_time')
-        self.append_data(self.mean_height, 'model_height')
-
-    def _get_model_type(self):
-        possible_keys = ('ecmwf', 'gdas')
-        for key in possible_keys:
-            if key in self.filename:
-                return key
-        return ''
-
-    def _get_model_heights(self, alt_site):
-        """Returns model heights for each time step."""
-        model_heights = self.dataset.variables['height']
-        if ma.count_masked(model_heights[:] > 0):
-            raise RuntimeError('Masked values in the data file! Aborting..')
-        return self.km2m(model_heights) + alt_site
-
-    def _get_mean_height(self):
-        return np.mean(np.array(self.model_heights), axis=0)
 
 
 COMMENTS = {
