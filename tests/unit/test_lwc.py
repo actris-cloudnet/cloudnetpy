062bb8f61c8de3e2064942560850563353dca122
Anniina Korpinen <anniina.korpinen@fmi.fi>
2019-11-12 14:15:35 +0200

    Starting developing tests for lwc

---
+++ tests/unit/test_lwc.py
@@ -0,0 +1,48 @@
+import numpy as np
+from collections import namedtuple
+from numpy import testing
+import pytest
+import netCDF4
+
+def test_get_echo():
+    assert True
+
+
+def test_get_liquid():
+    assert True
+
+
+def test_init_lwc_adiabatic():
+    assert True
+
+
+def test_adiabatic_lwc_to_lwc():
+    assert True
+
+
+def test_init_status():
+    assert True
+
+
+def test_adjust_clouds_to_match_lwp():
+    assert True
+
+
+def test_find_lwp_difference():
+    assert True
+
+
+def test_find_adjustable_clouds():
+    assert True
+
+
+def test_adjust_cloud_tops():
+    assert True
+
+
+def test_calc_lwc_error():
+    assert True
+
+
+def test_screen_rain():
+    assert True
