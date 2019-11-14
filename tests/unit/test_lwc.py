a229c7343d79661c81bb13dcb4f3b19b5e01fafd
Anniina Korpinen <anniina.korpinen@fmi.fi>
2019-11-14 13:21:47 +0200

    Starting developing tests for lwc

--- tests/unit/test_lwc.py
+++ tests/unit/test_lwc.py
@@ -3,6 +3,50 @@ from collections import namedtuple
 from numpy import testing
 import pytest
 import netCDF4
+from cloudnetpy.products.lwc import LwcSource, Lwc
+
+
+DIMENSIONS = ('time', 'height', 'model_time', 'model_height')
+TEST_ARRAY = np.arange(3)
+
+
+@pytest.fixture(scope='session')
+def lwc_source_file(tmpdir_factory, file_metadata):
+    file_name = tmpdir_factory.mktemp("data").join("file.nc")
+    root_grp = netCDF4.Dataset(file_name, "w", format="NETCDF4_CLASSIC")
+    _create_dimensions(root_grp)
+    _create_dimension_variables(root_grp)
+    var = root_grp.createVariable('altitude', 'f8')
+    var[:] = 1
+    var.units = 'km'
+    var = root_grp.createVariable('temperature', 'f8', ('time', 'height'))
+    var[:] = np.array([[282, 280, 278],
+                       [286, 284, 282],
+                      [284, 282, 280]])
+    var = root_grp.createVariable('pressure', 'f8', ('time', 'height'))
+    var[:] = np.array([[1010, 1000, 990],
+                       [1020, 1010, 1000],
+                      [1030, 1020, 1010]])
+    root_grp.close()
+    return file_name
+
+
+def _create_dimensions(root_grp):
+    n_dim = len(TEST_ARRAY)
+    for dim_name in DIMENSIONS:
+        root_grp.createDimension(dim_name, n_dim)
+
+
+def _create_dimension_variables(root_grp):
+    for dim_name in DIMENSIONS:
+        x = root_grp.createVariable(dim_name, 'f8', (dim_name,))
+        x[:] = TEST_ARRAY
+        if dim_name == 'height':
+            x.units = 'm'
+
+
+def test_get_atmosphere():
+    assert True
 
 
 def test_get_echo():
