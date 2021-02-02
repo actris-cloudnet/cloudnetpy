f49647dc3c7c0bfdd02022bcd3d7c61f12cdcd08
Simo Tukiainen <simo.tukiainen@fmi.fi>
2021-02-02 13:13:06 +0200

    Refactor

--- cloudnetpy/concat_lib.py
+++ cloudnetpy/concat_lib.py
@@ -6,7 +6,6 @@ from cloudnetpy import utils
 
 
 def concatenate_files(filenames: list, output_file: str,
-                      dimensions: tuple = ('range',),
                       variables: Union[list, None] = None,
                       new_attributes: Union[dict, None] = None) -> None:
     """Concatenate list of netCDF files in time dimension.
@@ -14,12 +13,10 @@ def concatenate_files(filenames: list, output_file: str,
     Args:
         filenames (list): List of files to be concatenated.
         output_file (str): Output file name.
-        dimensions (tuple, optional): Optional list of additional dimensions besides 'time' to be
-            included in the output file. Default is ('range',).
         variables (list, optional): List of variables with the 'time' dimension to be concatenated.
             Default is None when all variables with 'time' will be saved.
-        new_attributes (dict, optional): Optional new global attributes as {'attribute_name': value}.
-            The attribute value will be saved as string.
+        new_attributes (dict, optional): Optional new global attributes as
+            {'attribute_name': value}.
 
     Notes:
         Arrays without 'time' dimension, scalars, and global attributes will be taken from
@@ -27,8 +24,6 @@ def concatenate_files(filenames: list, output_file: str,
 
     """
     concat = Concat(filenames, output_file)
-    for dim in ('time',) + dimensions:
-        concat.create_dimension(dim)
     concat.get_constants()
     concat.create_global_attributes(new_attributes)
     concat.concat_data(variables)
@@ -39,23 +34,15 @@ class Concat:
     def __init__(self, filenames: list, output_file: str):
         self.filenames = sorted(filenames)
         self.first_file = netCDF4.Dataset(self.filenames[0])
-        self.concatenated_file = netCDF4.Dataset(output_file, 'w', format='NETCDF4_CLASSIC')
+        self.concatenated_file = self._init_output_file(output_file)
         self.constants = ()
-
-    def create_dimension(self, dim_name: str) -> None:
-        """Creates required dimensions, 'time' will be unlimited."""
-        if dim_name == 'time':
-            dim_len = None
-        else:
-            dim_len = len(self.first_file[dim_name])
-        self.concatenated_file.createDimension(dim_name, dim_len)
 
     def create_global_attributes(self, new_attributes: Union[dict, None] = None) -> None:
         """Copies global attributes from one of the source files."""
         _copy_attributes(self.first_file, self.concatenated_file)
         if new_attributes is not None:
             for key, value in new_attributes.items():
-                setattr(self.concatenated_file, key, str(value))
+                setattr(self.concatenated_file, key, value)
 
     def get_constants(self):
         """Finds constants, i.e. arrays that have no time dimension and are not concatenated."""
@@ -119,8 +106,16 @@ class Concat:
             variable_size += (dim,)
         return variable_size
 
+    def _init_output_file(self, output_file: str) -> netCDF4.Dataset:
+        nc = netCDF4.Dataset(output_file, 'w', format='NETCDF4_CLASSIC')
+        dimensions = [key for key in self.first_file.dimensions.keys()]
+        for dim in dimensions:
+            dim_len = None if dim == 'time' else len(self.first_file[dim])
+            nc.createDimension(dim, dim_len)
+        return nc
+
 
 def _copy_attributes(source, target) -> None:
     for attr in source.ncattrs():
         value = getattr(source, attr)
-        setattr(target, attr, str(value))
+        setattr(target, attr, value)
