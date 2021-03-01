6b2a6db2e149de7fad1a2dd232cfbc5f58b69559
Simo Tukiainen <simo.tukiainen@fmi.fi>
2021-03-01 14:01:52 +0200

    Refactor lwc.py

--- tests/unit/test_lwc.py
+++ tests/unit/test_lwc.py
@@ -3,7 +3,7 @@ from numpy.testing import assert_array_equal, assert_array_almost_equal
 from collections import namedtuple
 import pytest
 import netCDF4
-from cloudnetpy.products.lwc import LwcSource, Lwc, LwcStatus, LwcError
+from cloudnetpy.products.lwc import LwcSource, Lwc, CloudAdjustor, LwcError
 from cloudnetpy.categorize import atmos
 
 
@@ -59,30 +59,29 @@ def _create_dimension_variables(root_grp):
 
 def test_get_atmosphere_t(lwc_source_file):
     obj = LwcSource(lwc_source_file)
-    compare = np.array([[282, 280, 278],
-                        [286, 284, 282],
-                        [284, 282, 280]])
-    assert_array_equal(compare, obj.atmosphere[0])
+    expected = np.array([[282, 280, 278],
+                         [286, 284, 282],
+                         [284, 282, 280]])
+    assert_array_equal(obj.atmosphere[0], expected)
 
 
 def test_get_atmosphere_p(lwc_source_file):
     obj = LwcSource(lwc_source_file)
-    compare = np.array([[1010, 1000, 990],
-                        [1020, 1010, 1000],
-                        [1030, 1020, 1010]])
-    assert_array_equal(compare, obj.atmosphere[-1])
+    expected = np.array([[1010, 1000, 990],
+                         [1020, 1010, 1000],
+                         [1030, 1020, 1010]])
+    assert_array_equal(obj.atmosphere[-1], expected)
 
 
 class LwcSourceObj:
     def __init__(self):
         self.dheight = 10
-        self.categorize_bits = \
-            CategorizeBits(category_bits={'droplet': np.asarray([[1, 0, 1],
-                                                                 [0, 1, 1]], dtype=bool)},
-                           quality_bits={'radar': np.asarray([[1, 0, 1],
-                                                              [0, 1, 1]], dtype=bool),
-                                         'lidar': np.asarray([[1, 0, 1],
-                                                              [0, 1, 1]], dtype=bool)})
+        self.categorize_bits = CategorizeBits(category_bits={'droplet': np.asarray([[1, 0, 1],
+                                                                                    [0, 1, 1]], dtype=bool)},
+                                              quality_bits={'radar': np.asarray([[1, 0, 1],
+                                                                                 [0, 1, 1]], dtype=bool),
+                                                            'lidar': np.asarray([[1, 0, 1],
+                                                                                 [0, 1, 1]], dtype=bool)})
         self.atmosphere = (np.array([[282, 281, 280],
                                      [280, 279, 278]]),
                            np.array([[101000, 100500, 100000],
@@ -93,7 +92,7 @@ class LwcSourceObj:
 
 
 LWC_OBJ = Lwc(LwcSourceObj())
-STATUS_OBJ = LwcStatus(LwcSourceObj(), LWC_OBJ)
+STATUS_OBJ = CloudAdjustor(LwcSourceObj(), LWC_OBJ)
 ERROR_OBJ = LwcError(LwcSourceObj(), LWC_OBJ)
 
 
@@ -102,23 +101,19 @@ def test_get_liquid(value):
     assert value in LWC_OBJ.is_liquid
 
 
-def test_init_lwc_adiabatic(lwc_source_file):
+def test_init_lwc_adiabatic():
     lwc_source = LwcSourceObj()
-    compare = atmos.fill_clouds_with_lwc_dz(lwc_source.atmosphere,
-                                            LWC_OBJ.is_liquid)
-    compare[0, 0] *= 10
-    compare[0, 2] *= 10
-    compare[1, 1] *= 10
-    compare[1, 2] *= 20
-    assert_array_almost_equal(LWC_OBJ._init_lwc_adiabatic(), compare)
+    expected = atmos.fill_clouds_with_lwc_dz(lwc_source.atmosphere, LWC_OBJ.is_liquid)
+    expected[0, 0] *= 10
+    expected[0, 2] *= 10
+    expected[1, 1] *= 10
+    expected[1, 2] *= 20
+    assert_array_almost_equal(LWC_OBJ._init_lwc_adiabatic(), expected)
 
 
 def test_screen_rain_lwc():
-    compare = np.ma.array([[5, 1, 2],
-                           [3, 6, 0]],
-                          mask=[[0, 0, 0],
-                                [1, 1, 1]])
-    assert_array_equal(compare.mask, LWC_OBJ.lwc.mask)
+    expected = np.ma.array([[5, 1, 2], [3, 6, 0]], mask=[[0, 0, 0], [1, 1, 1]])
+    assert_array_equal(expected.mask, LWC_OBJ.lwc.mask)
 
 
 @pytest.mark.parametrize("value", [0, 1])
@@ -162,58 +157,58 @@ def test_find_adjustable_clouds():
 
 
 def test_find_topmost_clouds():
-    compare = np.asarray([[0, 0, 1], [0, 1, 1]], dtype=bool)
-    assert_array_equal(STATUS_OBJ._find_topmost_clouds(), compare)
+    expected = np.asarray([[0, 0, 1], [0, 1, 1]], dtype=bool)
+    assert_array_equal(STATUS_OBJ._find_topmost_clouds(), expected)
 
 
 def test_find_echo_combinations_in_liquid():
     STATUS_OBJ.echo['lidar'] = np.array([[0, 1, 0], [1, 1, 0]])
     STATUS_OBJ.echo['radar'] = np.array([[0, 0, 0], [0, 1, 1]])
     STATUS_OBJ.is_liquid = np.array([[1, 1, 1], [0, 1, 1]])
-    compare = np.array([[0, 1, 0], [0, 3, 2]])
-    assert_array_equal(STATUS_OBJ._find_echo_combinations_in_liquid(), compare)
+    expected = np.array([[0, 1, 0], [0, 3, 2]])
+    assert_array_equal(STATUS_OBJ._find_echo_combinations_in_liquid(), expected)
 
 
 def test_find_lidar_only_clouds():
     inds = np.array([[1, 0, 0], [0, 1, 3]])
-    compare = np.array([True, False])
-    assert_array_equal(STATUS_OBJ._find_lidar_only_clouds(inds), compare)
+    expected = np.array([True, False])
+    assert_array_equal(STATUS_OBJ._find_lidar_only_clouds(inds), expected)
 
 
 def test_remove_good_profiles():
     top_c = np.asarray([[1, 1, 0], [1, 0, 1]], dtype=bool)
-    compare = np.asarray([[1, 1, 0], [0, 0, 0]], dtype=bool)
-    assert_array_equal(STATUS_OBJ._remove_good_profiles(top_c), compare)
+    expected = np.asarray([[1, 1, 0], [0, 0, 0]], dtype=bool)
+    assert_array_equal(STATUS_OBJ._remove_good_profiles(top_c), expected)
 
 
 def test_find_lwp_difference():
     STATUS_OBJ.lwc_adiabatic = np.array([[1, 8, 2], [2, 3, 7]])
     STATUS_OBJ.lwc_source.lwp = np.array([50, 30])
-    compare = np.array([60, 90])
-    assert_array_equal(STATUS_OBJ._find_lwp_difference(), compare)
+    expected = np.array([60, 90])
+    assert_array_equal(STATUS_OBJ._find_lwp_difference(), expected)
 
 
 @pytest.mark.parametrize("value", [0, 1, 2, 3, 4])
 def test_screen_rain_status(value):
     STATUS_OBJ.lwc_source.is_rain = np.array([0, 1])
     STATUS_OBJ.status = np.array([[0, 2, 2, 3, 1], [1, 3, 0, 2, 2]])
-    STATUS_OBJ.screen_rain()
+    STATUS_OBJ._mask_rain()
     assert value in STATUS_OBJ.status
 
 
 def test_limit_error():
     error = np.array([[0, 0, 1], [0.2, 0.4, 0.3]])
     max_v = 0.5
-    compare = np.array([[0, 0, 0.5], [0.2, 0.4, 0.3]])
-    assert_array_equal(ERROR_OBJ._limit_error(error, max_v), compare)
+    expected = np.array([[0, 0, 0.5], [0.2, 0.4, 0.3]])
+    assert_array_equal(ERROR_OBJ._limit_error(error, max_v), expected)
 
 
 def test_calc_lwc_gradient():
     from cloudnetpy.utils import l2norm
     ERROR_OBJ.lwc = np.ma.array([[0.1, 0.2, 0.3],
                                  [0.1, 0.3, 0.6]])
-    compare = l2norm(*np.gradient(ERROR_OBJ.lwc))
-    assert_array_almost_equal(ERROR_OBJ._calc_lwc_gradient(), compare)
+    expected = l2norm(*np.gradient(ERROR_OBJ.lwc))
+    assert_array_almost_equal(ERROR_OBJ._calc_lwc_gradient(), expected)
 
 
 def test_calc_lwc_relative_error():
@@ -221,53 +216,50 @@ def test_calc_lwc_relative_error():
     ERROR_OBJ.lwc = np.ma.array([[0.1, 0.2, 0.3],
                                  [0.1, 0.3, 0.6]])
     x = l2norm(*np.gradient(ERROR_OBJ.lwc))
-    compare = x / ERROR_OBJ.lwc / 2
-    compare[compare > 5] = 5
-    assert_array_almost_equal(ERROR_OBJ._calc_lwc_relative_error(), compare)
+    expected = x / ERROR_OBJ.lwc / 2
+    expected[expected > 5] = 5
+    assert_array_almost_equal(ERROR_OBJ._calc_lwc_relative_error(), expected)
 
 
 def test_calc_lwp_relative_error():
     ERROR_OBJ.lwc_source.lwp = np.array([0.1, 0.5])
     ERROR_OBJ.lwc_source.lwp_error = np.array([0.2, 5.5])
-    compare = ERROR_OBJ.lwc_source.lwp_error / ERROR_OBJ.lwc_source.lwp
-    compare[compare > 10] = 10
-    assert_array_equal(ERROR_OBJ._calc_lwp_relative_error(), compare)
+    expected = ERROR_OBJ.lwc_source.lwp_error / ERROR_OBJ.lwc_source.lwp
+    expected[expected > 10] = 10
+    assert_array_equal(ERROR_OBJ._calc_lwp_relative_error(), expected)
 
 
 def test_calc_combined_error():
     from cloudnetpy.utils import transpose, l2norm
     err_2d = np.array([[0, 0.1, 0.1], [0.2, 0.4, 0.15]])
     err_1d = np.array([0.3, 0.2])
-    compare = l2norm(err_2d, transpose(err_1d))
-    assert_array_equal(ERROR_OBJ._calc_combined_error(err_2d, err_1d), compare)
+    expected = l2norm(err_2d, transpose(err_1d))
+    assert_array_equal(ERROR_OBJ._calc_combined_error(err_2d, err_1d), expected)
 
 
 def test_fill_error_array():
-    error_in = np.array([[0, 0.1, 0.1], [0.2, 0.4, 0.15]])
+    error_in = np.array([[0, 0.1, 0.1],
+                         [0.2, 0.4, 0.15]])
     ERROR_OBJ.lwc = np.ma.array([[0.1, 0.2, 0.1],
-                               [0.1, 0.2, 0.2]],
-                              mask=[[0, 1, 0],
-                                    [1, 0, 0]])
-    compare = np.ma.array([[0, 0, 0],
-                           [0, 0, 0]],
-                          mask=[[0, 1, 0],
-                                [1, 0, 0]])
+                                 [0.1, 0.2, 0.2]], mask=[[0, 1, 0],
+                                                         [1, 0, 0]])
+    expected = np.ma.array([[0, 0, 0],
+                            [0, 0, 0]], mask=[[0, 1, 0],
+                                              [1, 0, 0]])
     ERROR_OBJ._fill_error_array(error_in)
     error = ERROR_OBJ._fill_error_array(error_in)
-    assert_array_almost_equal(error.mask, compare.mask)
+    assert_array_almost_equal(error.mask, expected.mask)
 
 
 def test_screen_rain_error():
     compare = np.ma.array([[0.709, 0, 0.709],
-                           [0, 0, 0]],
-                          mask=[[0, 1, 0],
-                                [1, 1, 1]])
-    assert_array_equal(ERROR_OBJ.lwc_error.mask, compare.mask)
+                           [0, 0, 0]], mask=[[0, 1, 0],
+                                             [1, 1, 1]])
+    assert_array_equal(ERROR_OBJ.error.mask, compare.mask)
 
 
 @pytest.mark.parametrize("key", ["lwc", "lwc_retrieval_status", "lwc_error"])
 def test_append_data(lwc_source_file, key):
-    from cloudnetpy.products.lwc import _append_data
     lwc_source = LwcSource(lwc_source_file)
-    _append_data(lwc_source, LWC_OBJ, STATUS_OBJ, ERROR_OBJ)
+    lwc_source.append_results(LWC_OBJ.lwc, STATUS_OBJ.status, ERROR_OBJ.error)
     assert key in lwc_source.data.keys()
