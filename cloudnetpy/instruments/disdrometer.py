13b3f412c571634877583f34e64338b80fb4283e
Simo Tukiainen <simo.tukiainen@fmi.fi>
2021-09-23 19:18:52 +0300

    Fix metadata

--- cloudnetpy/instruments/disdrometer.py
+++ cloudnetpy/instruments/disdrometer.py
@@ -128,35 +128,6 @@ class Disdrometer:
         return CloudnetArray(utils.seconds2hours(seconds), 'time')
 
 
-class Thies(Disdrometer):
-
-    def __init__(self, filename: str, site_meta: dict):
-        super().__init__(filename, site_meta, THIES)
-        self.date = self._init_date()
-
-    def init_data(self):
-        column_and_key = [
-            (4, '_time'),
-            (13, 'rain_rate'),  # liquid
-            (14, 'snow_intensity'),
-            (16, 'visibility'),
-            (17, 'radar_reflectivity'),
-            (18, 'measurement_quality'),
-            (19, 'maximum_hail_diameter'),
-            (20, 'laser_status'),
-            (21, 'static_signal'),
-            (44, 'ambient_temperature'),
-            (49, 'n_particles')
-        ]
-        self._append_data(column_and_key)
-        self.data['spectrum'] = CloudnetArray(np.array(self._spectra), 'spectrum')
-
-    def _init_date(self) -> list:
-        first_date = self._file_contents[0][3]
-        first_date = _format_thies_date(first_date)
-        return first_date.split('-')
-
-
 class Parsivel(Disdrometer):
     def __init__(self, filename: str, site_meta: dict):
         super().__init__(filename, site_meta, PARSIVEL)
@@ -167,18 +138,25 @@ class Parsivel(Disdrometer):
     def init_data(self):
         column_and_key = [
             (0, '_time'),
-            (1, 'rain_rate'),  # liquid
-            (2, 'rain_accum'),
+            (1, 'rainfall_rate'),
+            #(2, 'rain_accum'),  # new
+            (3, 'synop_WaWa'),  # or synop_ww ?
             (4, 'radar_reflectivity'),
             (5, 'visibility'),
-            (7, 'signal_amplitude'),
+            (6, 'interval'),
+            (7, 'sig_laser'),
             (8, 'n_particles'),
-            (9, 'sensor_temperature'),
-            (12, 'heating_current'),
-            (13, 'sensor_voltage'),
+            (9, 'T_sensor'),
+            # (10, 'sensor_id'),
+            (12, 'I_heating'),
+            (13, 'V_sensor'),
+            (14, 'state_sensor'),
+            # (15, 'station_name'),
+            #(16, 'rain_amount_absolute'),  # new
+            (17, 'error_code')
         ]
         self._append_data(column_and_key)
-        self.data['spectrum'] = self._read_spectrum()
+        self.data['data_raw'] = self._read_spectrum()
 
     def _read_spectrum(self) -> CloudnetArray:
         n_velocity = 32
@@ -194,7 +172,7 @@ class Parsivel(Disdrometer):
                 except ValueError:
                     pass
             array[time_ind, :, :] = np.reshape(values, (32, 32))
-        return CloudnetArray(array, 'spectrum', dimensions=('time', 'velocity', 'diameter'))
+        return CloudnetArray(array, 'data_raw', dimensions=('time', 'velocity', 'diameter'))
 
     def _init_date(self) -> list:
         timestamp = self._file_contents[0][0]
@@ -231,6 +209,35 @@ class Parsivel(Disdrometer):
         return lower_limit, mid_value, upper_limit
 
 
+class Thies(Disdrometer):
+
+    def __init__(self, filename: str, site_meta: dict):
+        super().__init__(filename, site_meta, THIES)
+        self.date = self._init_date()
+
+    def init_data(self):
+        column_and_key = [
+            (4, '_time'),
+            (13, 'rain_rate'),  # liquid
+            (14, 'snow_intensity'),
+            (16, 'visibility'),
+            (17, 'radar_reflectivity'),
+            (18, 'measurement_quality'),
+            (19, 'maximum_hail_diameter'),
+            (20, 'laser_status'),
+            (21, 'static_signal'),
+            (44, 'ambient_temperature'),
+            (49, 'n_particles')
+        ]
+        self._append_data(column_and_key)
+        self.data['data_raw'] = CloudnetArray(np.array(self._spectra), 'data_raw')
+
+    def _init_date(self) -> list:
+        first_date = self._file_contents[0][3]
+        first_date = _format_thies_date(first_date)
+        return first_date.split('-')
+
+
 def save_disdrometer(disdrometer: Union[Parsivel, Thies],
                      output_file: str,
                      keep_uuid: bool,
@@ -242,7 +249,7 @@ def save_disdrometer(disdrometer: Union[Parsivel, Thies],
         dims['diameter'] = disdrometer.data['lower_diameter'][:].shape[0]
         dims['velocity'] = disdrometer.data['lower_velocity'][:].shape[0]
     else:
-        dims['spectrum'] = disdrometer.data['spectrum'][:].shape[1]
+        dims['data_raw'] = disdrometer.data['data_raw'][:].shape[1]
     file_type = 'disdrometer'
     rootgrp = output.init_file(output_file, dims, disdrometer.data, keep_uuid, uuid)
     file_uuid = rootgrp.file_uuid
@@ -274,32 +281,62 @@ def _format_thies_date(date: str):
 
 
 ATTRIBUTES = {
-    'precipitation_intensity': MetaData(
-        long_name='Precipitation intensity',
-        units='mm/h',
-        comment='Rain droplets only'
+    'rainfall_rate': MetaData(
+        long_name='Precipitation rate',
+        units='mm h-1',
+        comment='Variable 01 - Rain intensity (32 bit) 0000.000.'
     ),
     'radar_reflectivity': MetaData(
-        long_name='Radar reflectivity',
-        units='dBZ'
+        long_name='Equivalent radar reflectivity factor',
+        units='dBZ',
+        comment='Variable 07 - Radar reflectivity (32 bit).'
     ),
     'visibility': MetaData(
-        long_name='Meteorological optical range visibility',
-        units='m'
+        long_name='Visibility range in precipitation after MOR',
+        units='m',
+        comment='Variable 08 - MOR visibility in the precipitation.'
     ),
-    'number_of_particles': MetaData(
-        long_name='Number of detected particles',
+    'interval': MetaData(
+        long_name='Length of measurement interval',
+        comment='Variable 09 - Sample interval between two data retrieval request.'
     ),
-    'kinetic_energy': MetaData(
-        long_name='Kinetic energy',
+    'sig_laser': MetaData(
+        long_name='Signal amplitude of the laser',
+        comment='Variable 10 - Signal amplitude of the laser strip.'
     ),
-    'signal_amplitude': MetaData(
-        long_name='Signal amplitude',
+    'n_particles': MetaData(
+        long_name='Number of particles in time interval',
+        comment='Variable 11 - Number of detected particles.'
     ),
-    'sensor_temperature': MetaData(
-        long_name='Sensor temperature',
-        units='C'
+    'T_sensor': MetaData(
+        long_name='Temperature in the sensor',
+        units='C',
+        comment='Variable 12 - Temperature in the sensor.'
     ),
+    'I_heating': MetaData(
+        long_name='Heating current',
+        units='A',
+        comment='Variable 16 - Current through the heating system.'
+    ),
+    'V_sensor': MetaData(
+        long_name='Sensor voltage',
+        units='V',
+        comment='Variable 17 - Power supply voltage in the sensor.'
+    ),
+    'state_sensor': MetaData(
+        long_name='State of the sensor',
+        comment='Variable 18 - Sensor status: 0 = Everything is okay, 1 = Dirty, '
+                '2 = No measurement possible.'
+    ),
+    'error_code': MetaData(
+        long_name='Error code',
+        comment='Variable 25 - Error code.'
+    ),
+    'data_raw': MetaData(
+        long_name='Raw Data as a function of particle diameter and velocity.',
+        comment='Variable 93 - Raw data.'
+    ),
+    # Thies-specific:
     'ambient_temperature': MetaData(
         long_name='Ambient temperature',
         units='C'
@@ -316,9 +353,6 @@ ATTRIBUTES = {
         long_name='Snow intensity',
         units='mm/h'
     ),
-    'spectrum': MetaData(
-        long_name='Spectrum'
-    ),
     'measurement_quality': MetaData(
         long_name='Measurement quality',
         units='%'
@@ -334,5 +368,13 @@ ATTRIBUTES = {
     'static_signal': MetaData(
         long_name='Static signal',
         comment='0 = OK, 1 = ERROR'
+    ),
+    'kinetic_energy': MetaData(
+        long_name='Kinetic energy',
+    ),
+    'precipitation_intensity': MetaData(
+        long_name='Precipitation intensity',
+        units='mm/h',
+        comment='Rain droplets only'
     ),
 }
