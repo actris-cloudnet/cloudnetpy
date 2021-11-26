9ca27f08404c7dd613f843f4c550c3af5d16748f
Simo Tukiainen <simo.tukiainen@fmi.fi>
2021-11-26 12:13:40 +0200

    Drop python3.6

--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -1,10 +1,7 @@
-import os
 import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
-from zipfile import ZipFile
 from uuid import UUID
-import requests
 import importlib
 from cloudnetpy.instruments import mira2nc
 from cloudnetpy.instruments import ceilo2nc
