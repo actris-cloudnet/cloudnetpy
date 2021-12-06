c64a57455f8a77ff484304abf2b55deef717b123
Simo Tukiainen <simo.tukiainen@fmi.fi>
2021-12-06 10:27:02 +0200

    Improve melting layer detection without ldr

--- cloudnetpy/categorize/melting.py
+++ cloudnetpy/categorize/melting.py
@@ -1,12 +1,12 @@
 """Functions to find melting layer from data."""
-
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union
 import numpy as np
 import numpy.ma as ma
 from scipy.ndimage.filters import gaussian_filter
 from cloudnetpy.constants import T0
 from cloudnetpy.categorize import droplet
 from cloudnetpy.categorize.containers import ClassData
+from cloudnetpy import utils
 
 
 def find_melting_layer(obs: ClassData, smooth: Optional[bool] = True) -> np.ndarray:
@@ -48,63 +48,85 @@ def find_melting_layer(obs: ClassData, smooth: Optional[bool] = True) -> np.ndar
 
     melting_layer = np.zeros(obs.tw.shape, dtype=bool)
 
+    is_ldr = False
+    ldr_prof = None
+    ldr_dprof = None
+    ldr_diff = None
+
     if hasattr(obs, 'ldr'):
         ldr_diff = np.diff(obs.ldr, axis=1).filled(0)
         is_ldr = True
-    else:
-        is_ldr = False
-        ldr_prof = None
-        ldr_dprof = None
 
-    v_diff = np.diff(obs.v, axis=1).filled(0)
     t_range = _find_model_temperature_range(obs.model_type)
 
     for ind, t_prof in enumerate(obs.tw):
 
         temp_indices = _get_temp_indices(t_prof, t_range)
+        z_prof = obs.z[ind, temp_indices]
+        height = obs.height[temp_indices]
+        width_prof = obs.width[ind, temp_indices]
 
-        if is_ldr:
+        if is_ldr is True:
             ldr_prof = obs.ldr[ind, temp_indices]
             ldr_dprof = ldr_diff[ind, temp_indices]
 
         v_prof = obs.v[ind, temp_indices]
-        v_dprof = v_diff[ind, temp_indices]
 
         if ma.count(ldr_prof) > 3 or ma.count(v_prof) > 3:
+
             try:
-                if is_ldr:
-                    ldr_peak = np.argmax(ldr_prof)
+                if is_ldr is True:
+                    ldr_peak = int(np.argmax(ldr_prof))
                 else:
                     raise ValueError
                 base, top = _basetop(ldr_dprof, ldr_peak)
-                if _is_good_ldr_peak(ldr_prof, v_prof, (base, ldr_peak, top)):
+
+                if _is_good_ldr_peak(ldr_prof, v_prof, z_prof, (base, ldr_peak, top)):
                     melting_layer[ind, temp_indices[ldr_peak-1]:temp_indices[top]+1] = True
+
             except (ValueError, IndexError):
-                v_peak = np.argmax(v_dprof)
-                try:
-                    base, top = _basetop(v_dprof, v_peak)
-                    if _is_good_v_peak(v_prof, base, top):
-                        melting_layer[ind, temp_indices[v_peak-1:v_peak+2]] = True
-                except (ValueError, IndexError):
-                    continue
+
+                indices = _find_melting_layer_from_v(v_prof, width_prof, height)
+                if indices is not None:
+                    melting_layer[ind, indices] = True
+
     if smooth:
         smoothed_layer = gaussian_filter(np.array(melting_layer, dtype=float), (2, 0.1))
         melting_layer = (smoothed_layer > 0.2).astype(bool)
+
     return melting_layer
 
 
-def _is_good_v_peak(v: np.ndarray, base: int, top: int) -> bool:
-    diff = v[top] - v[base]
-    return diff > 0.5 and v[base] < -2
+def _find_melting_layer_from_v(v_prof: np.ndarray,
+                               width_prof: np.ndarray,
+                               height: np.ndarray) -> Union[np.ndarray, None]:
+    v = np.copy(v_prof[:-1])
+    v_diff = np.diff(v_prof)
+    v[v_diff < 0] = 0
+    v[v_diff > 0] = 1
+    n_increasing = utils.cumsumr(v)
+    try:
+        top = int(np.argmax(n_increasing))
+        base = np.where(n_increasing[:top] == 0)[0][-1]
+    except IndexError:
+        return None
+    conditions = (width_prof[base] - width_prof[top] > 0.3,
+                  v_prof[top] - v_prof[base] > 1,
+                  50 < (height[top] - height[base]) < 300,
+                  v_prof[base] < -1)
+    if all(conditions):
+        return np.arange(base, top)
 
 
 def _is_good_ldr_peak(ldr: np.ndarray,
                       v: np.ndarray,
+                      z: np.ndarray,
                       indices: Tuple[int, int, int]) -> bool:
     base, peak, top = indices
     conditions = (ldr[peak] - ldr[top] > 3,
                   ldr[peak] - ldr[base] > 4,
                   ldr[peak] > -30,
+                  z[base] > -25,
                   v[base] < -1)
     return all(conditions)
 
@@ -116,7 +138,7 @@ def _basetop(dprof: np.ndarray, pind: int) -> Tuple[int, int]:
     return base, top
 
 
-def _get_temp_indices(t_prof: np.ndarray, t_range: np.ndarray) -> np.ndarray:
+def _get_temp_indices(t_prof: np.ndarray, t_range: tuple) -> np.ndarray:
     """Finds indices of temperature profile covering the given range."""
     bottom_point = np.where(t_prof < (T0 - t_range[0]))[0][0]
     top_point = np.where(t_prof > (T0 + t_range[0]))[0]
