6312273a008eb489cc00b069697239420e2fee02
Simo Tukiainen <simo.tukiainen@fmi.fi>
2022-11-30 16:29:39 +0200

    Update `cloudnetpy-qc` version requirement

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         "netCDF4",
         "matplotlib",
         "requests",
-        "cloudnetpy_qc>=1.1.1",
+        "cloudnetpy_qc>=1.4.0",
         "scikit-image",
         "rpgpy>=0.12.1",
     ],
