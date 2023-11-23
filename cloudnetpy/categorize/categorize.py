9823e03c4d5b920471fbf5759b1263aeec031acd
Simo Tukiainen <simo.tukiainen@fmi.fi>
2023-11-23 11:21:09 +0200

    Fix bug in time index order

--- cloudnetpy/categorize/categorize.py
+++ cloudnetpy/categorize/categorize.py
@@ -68,8 +68,8 @@ def generate_categorize(
         model_gap_ind = data["model"].interpolate_to_grid(time, height)
         radar_gap_ind = data["radar"].rebin_to_grid(time)
         lidar_gap_ind = data["lidar"].interpolate_to_grid(time, height)
-        gap_indices = radar_gap_ind + lidar_gap_ind + model_gap_ind
-        return list(set(range(len(time))) - set(gap_indices))
+        gap_indices = set(radar_gap_ind + lidar_gap_ind + model_gap_ind)
+        return [ind for ind in range(len(time)) if ind not in gap_indices]
 
     def _screen_bad_time_indices(valid_indices: list) -> None:
         n_time_full = len(time)
