4e2116591f104ec18dde600efafd8b4cf35d3b85
Tuomas Siipola <tuomas.siipola@fmi.fi>
2024-11-15 13:38:58 +0200

    Add height to Radiometrics files

--- cloudnetpy/instruments/radiometrics.py
+++ cloudnetpy/instruments/radiometrics.py
@@ -93,7 +93,7 @@ class Radiometrics:
         self.raw_data: list[Record] = []
         self.data: dict = {}
         self.instrument = instruments.RADIOMETRICS
-        self.heights: list[str] = []
+        self.ranges: list[str] = []
 
     def read_raw_data(self) -> None:
         """Reads Radiometrics raw data."""
@@ -111,7 +111,7 @@ class Radiometrics:
                     columns = row[3:]
                     record_columns[record_type] = columns
                     if record_type in (10, 400):
-                        self.heights = [
+                        self.ranges = [
                             column
                             for column in columns
                             if re.fullmatch(r"\d+\.\d+", column)
@@ -155,16 +155,12 @@ class Radiometrics:
             if title := block_titles.get(record.block_type + record.block_index):
                 if title == "Temperature (K)":
                     temps.append(
-                        [float(record.values[column]) for column in self.heights]
+                        [float(record.values[column]) for column in self.ranges]
                     )
                 elif title == "Relative Humidity (%)":
-                    rhs.append(
-                        [float(record.values[column]) for column in self.heights]
-                    )
+                    rhs.append([float(record.values[column]) for column in self.ranges])
                 elif title == "Vapor Density (g/m^3)":
-                    ahs.append(
-                        [float(record.values[column]) for column in self.heights]
-                    )
+                    ahs.append([float(record.values[column]) for column in self.ranges])
             elif record.block_type == 10:
                 if record.block_index == 0:
                     lwp = record.values["Lqint(mm)"]
@@ -175,16 +171,12 @@ class Radiometrics:
                     iwvs.append(float(iwv))
                     irts.append([float(irt)])
                     temps.append(
-                        [float(record.values[column]) for column in self.heights]
+                        [float(record.values[column]) for column in self.ranges]
                     )
                 elif record.block_index == 1:
-                    ahs.append(
-                        [float(record.values[column]) for column in self.heights]
-                    )
+                    ahs.append([float(record.values[column]) for column in self.ranges])
                 elif record.block_index == 2:
-                    rhs.append(
-                        [float(record.values[column]) for column in self.heights]
-                    )
+                    rhs.append([float(record.values[column]) for column in self.ranges])
             elif record.block_type == 200:
                 irt = record.values["Tir(K)"]
                 irts.append([float(irt)])
@@ -214,13 +206,14 @@ class RadiometricsCombined:
         self.data = {}
         self.date = None
         for obj in objs:
-            if obj.heights != objs[0].heights:
-                msg = "Inconsistent heights between files"
+            if obj.ranges != objs[0].ranges:
+                msg = "Inconsistent range between files"
                 raise InconsistentDataError(msg)
             for key in obj.data:
                 self.data = utils.append_data(self.data, key, obj.data[key])
-        heights = [float(x) for x in objs[0].heights]
-        self.data["range"] = np.array(heights) * 1000  # m => km
+        ranges = [float(x) for x in objs[0].ranges]
+        self.data["range"] = np.array(ranges) * 1000  # m => km
+        self.data["height"] = self.data["range"] + self.site_meta["altitude"]
         self.instrument = instruments.RADIOMETRICS
 
     def screen_time(self, expected_date: datetime.date | None) -> None:
@@ -233,7 +226,7 @@ class RadiometricsCombined:
         if np.count_nonzero(valid_mask) == 0:
             raise ValidTimeStampError
         for key in self.data:
-            if key == "range":
+            if key in ("range", "height"):
                 continue
             self.data[key] = self.data[key][valid_mask]
 
