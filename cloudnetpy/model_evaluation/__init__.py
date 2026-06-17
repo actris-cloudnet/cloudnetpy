fdfab897d4af9b7b086760f12ff996c700695101
Simo Tukiainen <simo.tukiainen@fmi.fi>
2026-06-17 13:49:06 +0300

    Expose model_evaluation entry points and use shorter imports

---
+++ cloudnetpy/model_evaluation/__init__.py
@@ -0,0 +1,6 @@
+from cloudnetpy.model_evaluation.plotting.plotting import generate_L3_day_plots
+from cloudnetpy.model_evaluation.products.product_resampling import (
+    process_L3_day_product,
+)
+
+__all__ = ["generate_L3_day_plots", "process_L3_day_product"]
